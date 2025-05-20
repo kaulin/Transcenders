be73935e5d7e654381b137efbda6dcb0044fc1e9
makelannika <makelannika@gmail.com>
2025-05-20 14:30:36 +0300

    new path for index.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
-import './index.css'
+import './styles/index.css'
 import './i18n/i18n'
 import { UserProvider } from './contexts/UserContext.tsx'
 
