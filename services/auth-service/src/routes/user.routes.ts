eec2ca5077ae5dbb09293bcd4892cfd806219a3e
aklein <aklein@student.hive.fi>
2025-06-02 00:08:08 +0300

    feat(auth-service): routes documentation in user.routes.ts

--- services/auth-service/src/routes/user.routes.ts
+++ services/auth-service/src/routes/user.routes.ts
@@ -6,12 +6,18 @@ export async function registerUserRoutes(app: FastifyInstance) {
   app.get('/api/users', UserController.getUsers);
   app.post('/api/users', { schema: createUserSchema }, UserController.addUser);
   app.get('/api/users/:id', UserController.getUserById);
+  app.get('/api/users/search', UserController.getUser);
   app.patch('/api/users/:id', { schema: updateUserSchema }, UserController.updateUser);
   app.delete('/api/users/:id', UserController.deleteUser);
-  app.get('/api/users/search', UserController.getUser);
 }
 
-/** add user
+/** get all users
+
+curl -X GET http://localhost:3001/api/users
+
+*/
+
+/** add user (ie: add a user specified in body)
 
 curl -X POST http://localhost:3001/api/users \
   -H "Content-Type: application/json" \
@@ -19,16 +25,29 @@ curl -X POST http://localhost:3001/api/users \
 
  */
 
-/** delete user at ID 3
+/** get a user by id (ie: id 2)
 
-curl -X DELETE http://localhost:3001/api/users/3
+curl -X GET http://localhost:3001/api/users/2
 
- */
+*/
 
-/** modify user (change display name to Allar on user ID 1)
+/** get a user by searching with query params (username or email)
+
+curl -X GET http://localhost:3001/api/users/search?username=allar
+curl -X GET http://localhost:3001/api/users/search?email=allar@example.com
+
+*/
+
+/** update user (ie: change display name to allar on user ID 1)
 
 curl -X PATCH http://localhost:3001/api/users/1 \
-  -H "Content-Type: application/json" \
-  -d '{"display_name": "allar"}'
+-H "Content-Type: application/json" \
+-d '{"display_name": "allar"}'
+
+*/
+
+/** delete user (ie: ID 3)
+
+curl -X DELETE http://localhost:3001/api/users/3
 
  */
