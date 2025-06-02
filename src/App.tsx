ee2a532fba5d8656f1a3d651fe79d40eb804dc2e
Annika <makelannika@gmail.com>
2025-06-03 02:06:34 +0300

    moved content to main container

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,43 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+// import { Routes, Route, Navigate } from 'react-router-dom'
 
-import { useUser } from "./contexts/UserContext"
+// import { useUser } from "./contexts/UserContext"
 
 import Header from './layout/Header'
-import Footer from './layout/Footer'
 import MainContainer from './layout/MainContainer'
+// import Footer from './layout/Footer'
 
-import Login from './pages/Login'
-import SignUp from './pages/SignUp'
-import Home from './pages/Home'
-import Tournament from './pages/Tournament'
-import Profile from './pages/Profile'
-import Settings from './pages/Settings'
-import LocalGame from './pages/LocalGame'
+// import Login from './pages/Login'
+// import SignUp from './pages/SignUp'
+// import Home from './pages/Home'
+// import Tournament from './pages/Tournament'
+// import Profile from './pages/Profile'
+// import Settings from './pages/Settings'
+// import LocalGame from './pages/LocalGame'
 
-import Background from './components/Background'
+// import Background from './components/Background'
 
-const bubbles = [
-  { id: 1, colStart: 1, rowStart: 1, colSpan: 2, rowSpan: 2, className: "bg-bubble-8" },
-  { id: 1, colStart: 4, rowStart: 1, colSpan: 2, rowSpan: 2, className: "bg-bubble-4" },
-  { id: 1, colStart: 6, rowStart: 1, colSpan: 3, rowSpan: 3, className: "bg-bubble-6" },
-  { id: 1, colStart: 10, rowStart: 1, colSpan: 2, rowSpan: 2, className: "bg-bubble-8" },
-  { id: 1, colStart: 12, rowStart: 1, colSpan: 1, rowSpan: 1, className: "bg-bubble-4" },
-  { id: 1, colStart: 4, rowStart: 2, colSpan: 2, rowSpan: 2, className: "bg-bubble-6" },
-  { id: 1, colStart: 8, rowStart: 2, colSpan: 2, rowSpan: 2, className: "bg-bubble-8" },
-  { id: 1, colStart: 11, rowStart: 2, colSpan: 2, rowSpan: 2, className: "bg-bubble-8" },
-  { id: 1, colStart: 3, rowStart: 3, colSpan: 2, rowSpan: 2, className: "bg-bubble-8" },
-]
+// const bubbles = [
+//   { id: 1, colStart: 1, rowStart: 1, colSpan: 2, rowSpan: 2, className: "bg-bubble-8" },
+//   { id: 1, colStart: 4, rowStart: 1, colSpan: 2, rowSpan: 2, className: "bg-bubble-4" },
+//   { id: 1, colStart: 6, rowStart: 1, colSpan: 3, rowSpan: 3, className: "bg-bubble-6" },
+//   { id: 1, colStart: 10, rowStart: 1, colSpan: 2, rowSpan: 2, className: "bg-bubble-8" },
+//   { id: 1, colStart: 12, rowStart: 1, colSpan: 1, rowSpan: 1, className: "bg-bubble-4" },
+//   { id: 1, colStart: 4, rowStart: 2, colSpan: 2, rowSpan: 2, className: "bg-bubble-6" },
+//   { id: 1, colStart: 8, rowStart: 2, colSpan: 2, rowSpan: 2, className: "bg-bubble-8" },
+//   { id: 1, colStart: 11, rowStart: 2, colSpan: 2, rowSpan: 2, className: "bg-bubble-8" },
+//   { id: 1, colStart: 3, rowStart: 3, colSpan: 2, rowSpan: 2, className: "bg-bubble-8" },
+// ]
 
 const App = () => {
-  const { user } = useUser()
 
   return (
-    <div className="relative h-screen overflow-hidden">
+    <div className="relative w-screen h-screen overflow-hidden">
 		{/* <Background bubbles={bubbles} /> */}
-		<div className="col-start-10 row-start-1 col-span-2 row-span-2 bg-red-500" />
 
-		<main>
+		<div className="relative z-10 flex flex-col h-full">
 			<Header />
-			{user ? (
-			<>
-				<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/localgame" element={<LocalGame />} />
-				<Route path="/tournament" element={<Tournament />} />
-				<Route path="/profile" element={<Profile />} />
-				<Route path="/settings" element={<Settings />} />
-				<Route path="*" element={<Navigate to="/" />} />
-				</Routes>
-			</>
-			) : (
-			<>
-				<Routes>
-				<Route path="/login" element={<Login />} />
-				<Route path="/signup" element={<SignUp />} />
-				<Route path="*" element={<Navigate to="/login" />} />
-				</Routes>
-			</>
-			)}
-		</main>
+			<MainContainer />	
+		</div>
     </div>
   )
 }
--- src/layout/MainContainer.tsx
+++ src/layout/MainContainer.tsx
@@ -1,9 +1,38 @@
-import type { ReactNode } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom'
 
-export default function MainContainer({ children }: { children: ReactNode }) {
+import { useUser } from '../contexts/UserContext';
+
+import Login from '../pages/Login'
+import SignUp from '../pages/SignUp'
+import Home from '../pages/Home'
+import Profile from '../pages/Profile'
+import Settings from '../pages/Settings'
+import Tournament from '../pages/Tournament'
+import LocalGame from '../pages/LocalGame'
+
+const MainContainer = () => {
+  const { user } = useUser()
+
   return (
-    <main className="max-w-4xl mx-auto p-4">
-      {children}
-    </main>
+    <div className="mt-24 h-[calc(100%-5rem)] overflow-y-auto px-10 pb-8">
+      {user ? (
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/localgame" element={<LocalGame />} />
+            <Route path="/tournament" element={<Tournament />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        ) : (
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="*" element={<Navigate to="/login" />} />
+          </Routes>
+			)}
+    </div>
   );
 }
+
+export default MainContainer
