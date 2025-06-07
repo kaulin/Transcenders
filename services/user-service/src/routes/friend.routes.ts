429720d4f40197d9616910b3f2cb954a9d630576
aklein <aklein@student.hive.fi>
2025-06-07 04:15:21 +0300

    feat(friendship): implement standardized API response handling and refactor service methods

--- services/user-service/src/routes/friend.routes.ts
+++ services/user-service/src/routes/friend.routes.ts
@@ -3,6 +3,7 @@ import {
   paramsIdSchema,
   removeFriendSchema,
   requestFriendSchema,
+  standardApiResponses,
 } from '@transcenders/contracts';
 import { FastifyInstance } from 'fastify';
 import { FriendshipController } from '../controllers/FriendshipController';
@@ -10,32 +11,63 @@ import { FriendshipController } from '../controllers/FriendshipController';
 export async function registerFriendshipRoutes(app: FastifyInstance) {
   app.get(
     FRIENDSHIP_ROUTES.USER_FRIENDS,
-    { schema: paramsIdSchema },
+    {
+      schema: {
+        params: paramsIdSchema.params,
+        response: standardApiResponses,
+      },
+    },
+
     FriendshipController.getFriends,
   );
   app.delete(
     FRIENDSHIP_ROUTES.REMOVE_FRIEND,
-    { schema: removeFriendSchema },
+    {
+      schema: {
+        body: removeFriendSchema.body,
+        response: standardApiResponses,
+      },
+    },
     FriendshipController.removeFriend,
   );
   app.post(
     FRIENDSHIP_ROUTES.SEND_REQUEST,
-    { schema: requestFriendSchema },
+    {
+      schema: {
+        body: requestFriendSchema.body,
+        response: standardApiResponses,
+      },
+    },
     FriendshipController.sendRequest,
   );
   app.get(
     FRIENDSHIP_ROUTES.INCOMING_REQUESTS,
-    { schema: paramsIdSchema },
+    {
+      schema: {
+        params: paramsIdSchema.params,
+        response: standardApiResponses,
+      },
+    },
     FriendshipController.getIncomingFriendRequests,
   );
   app.post(
     FRIENDSHIP_ROUTES.FRIEND_ACCEPT,
-    { schema: paramsIdSchema },
+    {
+      schema: {
+        params: paramsIdSchema.params,
+        response: standardApiResponses,
+      },
+    },
     FriendshipController.acceptFriend,
   );
   app.post(
     FRIENDSHIP_ROUTES.FRIEND_DECLINE,
-    { schema: paramsIdSchema },
+    {
+      schema: {
+        params: paramsIdSchema.params,
+        response: standardApiResponses,
+      },
+    },
     FriendshipController.declineFriend,
   );
 }
