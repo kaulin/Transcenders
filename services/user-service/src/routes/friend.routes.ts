372ba53937d8a2e7cd70d959b068304a094b289d
aklein <aklein@student.hive.fi>
2025-06-08 04:47:19 +0300

    refactor(routes): comment about swagger docs

--- services/user-service/src/routes/friend.routes.ts
+++ services/user-service/src/routes/friend.routes.ts
@@ -12,6 +12,10 @@ import {
 import { FastifyInstance } from 'fastify';
 import { FriendshipController } from '../controllers/FriendshipController';
 
+/**
+ * SWAGGER
+ * localhost:3001/docs
+ */
 export async function registerFriendshipRoutes(app: FastifyInstance) {
   app.get(
     FRIENDSHIP_ROUTES.USER_FRIENDSHIPS,
@@ -103,43 +107,3 @@ export async function registerFriendshipRoutes(app: FastifyInstance) {
     FriendshipController.checkFriendshipExists,
   );
 }
-
-/** get user's friends (ie: get friends for user ID 1)
-
-curl -X GET http://localhost:3001/users/1/friends
-
-*/
-
-/** remove friendship
-
-curl -X DELETE http://localhost:3001/friend/remove \
-  -H "Content-Type: application/json" \
-  -d '{"user1_id": 1, "user2_id": 2}'
-
-*/
-
-/** send friend request
-
-curl -X POST http://localhost:3001/friend/request \
-  -H "Content-Type: application/json" \
-  -d '{"initiator_id": 1, "recipient_id": 2}'
-
-*/
-
-/** get incoming friend requests (ie: for user ID 1)
-
-curl -X GET http://localhost:3001/friend/requests/1
-
-*/
-
-/** accept friend request (ie: accept request ID 5)
-
-curl -X POST http://localhost:3001/friend/accept/5
-
-*/
-
-/** decline friend request (ie: decline request ID 5)
-
-curl -X POST http://localhost:3001/friend/decline/5
-
-*/
--- services/user-service/src/routes/user.routes.ts
+++ services/user-service/src/routes/user.routes.ts
@@ -11,6 +11,10 @@ import {
 import { FastifyInstance } from 'fastify';
 import { UserController } from '../controllers/UserController';
 
+/**
+ * SWAGGER
+ * localhost:3001/docs
+ */
 export async function registerUserRoutes(app: FastifyInstance) {
   app.get(
     USER_ROUTES.USERS,
@@ -104,52 +108,3 @@ export async function registerUserRoutes(app: FastifyInstance) {
     UserController.getUserExact,
   );
 }
-
-/** get all users
-
-curl -X GET http://localhost:3001/users
-
-*/
-
-/** add user (ie: add a user specified in body)
-
-curl -X POST http://localhost:3001/users/create \
-  -H "Content-Type: application/json" \
-  -d '{"username": "allar", "email": "allar@example.com"}'
-
- */
-
-/** check if user exists (param :identifier can be username or email)
-
-curl -X GET http://localhost:3001/users/check/imnothere
-curl -X GET http://localhost:3001/users/check/allar
-curl -X GET http://localhost:3001/users/check/allar@example.com
-
-*/
-
-/** get a user by id (ie: id 2)
-
-curl -X GET http://localhost:3001/users/2
-
-*/
-
-/** get a user by searching with query params (username or email)
-
-curl -X GET http://localhost:3001/users/search?username=allar
-curl -X GET http://localhost:3001/users/search?email=allar@example.com
-
-*/
-
-/** update user (ie: change display name to allar on user ID 1) NB! invalid keys get scrapped
-
-curl -X PATCH http://localhost:3001/users/update/1 \
--H "Content-Type: application/json" \
--d '{"display_name": "allar"}'
-
-*/
-
-/** delete user (ie: ID 3)
-
-curl -X DELETE http://localhost:3001/users/remove/3
-
- */
