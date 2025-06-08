48c4d47e622ead703e02aa42bce29d4144efdf48
aklein <aklein@student.hive.fi>
2025-06-08 04:44:05 +0300

    refactor(friendship): enhance friendship routes and schemas for clarity and consistency

--- services/user-service/src/controllers/FriendshipController.ts
+++ services/user-service/src/controllers/FriendshipController.ts
@@ -1,8 +1,11 @@
 import {
-  CheckFriendshipExistsParams,
-  ParamsIdRequest,
+  AcceptFriendRequest,
+  CheckFriendshipExistsRequest,
+  DeclineFriendRequest,
+  GetFriendsRequest,
+  GetsRequestsRequest as GetRequests,
   RemoveFriendRequest,
-  RequestFriendRequest,
+  SendFriendRequestRequest,
 } from '@transcenders/contracts';
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { FriendshipService } from '../services/FriendshipService';
@@ -10,53 +13,57 @@ import { ResponseHelper } from '../utils/responseHelper';
 
 export class FriendshipController {
   static async getFriends(request: FastifyRequest, reply: FastifyReply) {
-    const { id } = request.params as ParamsIdRequest;
+    const { id } = request.params as GetFriendsRequest;
     const userId = parseInt(id);
 
     const result = await FriendshipService.getUserFriends(userId);
     return ResponseHelper.handleDatabaseResult(reply, result);
   }
 
-  static async sendRequest(request: FastifyRequest, reply: FastifyReply) {
-    const { initiator_id, recipient_id } = request.body as RequestFriendRequest;
+  static async removeFriend(request: FastifyRequest, reply: FastifyReply) {
+    const { id, friendId } = request.params as RemoveFriendRequest;
 
-    const result = await FriendshipService.sendFriendRequest(initiator_id, recipient_id);
+    const user_id = parseInt(id);
+    const friend_id = parseInt(friendId);
+    const result = await FriendshipService.removeFriend(user_id, friend_id);
     return ResponseHelper.handleDatabaseResult(reply, result);
   }
 
-  static async getIncomingFriendRequests(request: FastifyRequest, reply: FastifyReply) {
-    const { id } = request.params as ParamsIdRequest;
+  static async getRequests(request: FastifyRequest, reply: FastifyReply) {
+    const { id } = request.params as GetRequests;
     const userId = parseInt(id);
 
     const result = await FriendshipService.getIncomingFriendRequests(userId);
     return ResponseHelper.handleDatabaseResult(reply, result);
   }
 
+  static async sendRequest(request: FastifyRequest, reply: FastifyReply) {
+    const { id, recipientId } = request.params as SendFriendRequestRequest;
+    const user_id = parseInt(id);
+    const recipient_id = parseInt(recipientId);
+
+    const result = await FriendshipService.sendFriendRequest(user_id, recipient_id);
+    return ResponseHelper.handleDatabaseResult(reply, result);
+  }
+
   static async acceptFriend(request: FastifyRequest, reply: FastifyReply) {
-    const { id } = request.params as ParamsIdRequest;
-    const friendRequestId = parseInt(id);
+    const { requestId } = request.params as AcceptFriendRequest;
+    const friendRequestId = parseInt(requestId);
 
     const result = await FriendshipService.acceptFriend(friendRequestId);
     return ResponseHelper.handleDatabaseResult(reply, result);
   }
 
   static async declineFriend(request: FastifyRequest, reply: FastifyReply) {
-    const { id } = request.params as ParamsIdRequest;
-    const friendRequestId = parseInt(id);
+    const { requestId } = request.params as DeclineFriendRequest;
+    const friendRequestId = parseInt(requestId);
 
     const result = await FriendshipService.declineFriend(friendRequestId);
     return ResponseHelper.handleDatabaseResult(reply, result);
   }
 
-  static async removeFriend(request: FastifyRequest, reply: FastifyReply) {
-    const { user_1, user_2 } = request.body as RemoveFriendRequest;
-
-    const result = await FriendshipService.removeFriend(user_1, user_2);
-    return ResponseHelper.handleDatabaseResult(reply, result);
-  }
-
   static async checkFriendshipExists(request: FastifyRequest, reply: FastifyReply) {
-    const { id1, id2 } = request.params as CheckFriendshipExistsParams;
+    const { id1, id2 } = request.params as CheckFriendshipExistsRequest;
 
     const userId1 = parseInt(id1);
     const userId2 = parseInt(id2);
