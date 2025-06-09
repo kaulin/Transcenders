9839f3edf29846b9afccb8a378458a90b7a396fd
aklein <aklein@student.hive.fi>
2025-06-10 00:56:53 +0300

    feat: testing and seeing how auth works, think I get it

--- services/auth-service/src/services/auth.service.ts
+++ services/auth-service/src/services/auth.service.ts
@@ -1,5 +1,9 @@
-import { ApiClient, User } from '@transcenders/contracts';
+import { ApiClient, DatabaseHelper, DB_ERROR_CODES, User } from '@transcenders/contracts';
+import * as bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import SQL from 'sql-template-strings';
+import { Database } from 'sqlite';
+import { getAuthDB } from '../db/database';
 
 export interface JWTPayload {
   userId: number;
@@ -15,11 +19,49 @@ export class AuthService {
     if (!apiResponse.success) {
       throw new Error(`Authentication failed: ${apiResponse.error}`);
     }
-
     const userData = apiResponse.data as User;
 
+    const salt = await bcrypt.genSalt(10);
+    const pw_hash = await bcrypt.hash(password, salt);
+
+    const db: Database = await getAuthDB();
+    const insert = await DatabaseHelper.executeQuery<boolean>(
+      'creating user_credentials entry for fun',
+      db,
+      async (database) => {
+        const sql = SQL`
+        INSERT INTO user_credentials (user_id, username, email, pw_hash)
+        VALUES (${userData.id}, ${username}, ${userData.email}, ${pw_hash})
+      `;
+        const result = await database.run(sql.text, sql.values);
+        if (!result.lastID) {
+          throw new Error('Failed to create user');
+        }
+        return true;
+      },
+    );
+
+    const auth_data = await DatabaseHelper.executeQuery<any>(
+      'auth: get user by username',
+      db,
+      async (database) => {
+        const sql = SQL`
+        SELECT * FROM user_credentials WHERE username = ${username}
+      `;
+        const user = await database.get(sql.text, sql.values);
+        if (!user) {
+          const error = new Error(`user '${username}' not found`);
+          (error as any).code = DB_ERROR_CODES.RECORD_NOT_FOUND;
+          throw error;
+        }
+        return user;
+      },
+    );
+
+    const wrongpw = '$2b$10$edmS4rwIv5tew1Qq0yg1GOevP2kDsSqI02v.INKteFOpcFsadahTZre4a';
     // Verify hashed pw with bcrypt, might need to has password before function calling also?
-    const isValidPassword = true; //await bcrypt.compare(password, user.data.passwordHash);
+    const isValidPassword = await bcrypt.compare(password, auth_data.data.pw_hash);
+    // const isValidPassword = await bcrypt.compare(password, wrongpw);
     if (!isValidPassword) {
       throw new Error('Invalid password');
     }
