21d92f97a14f8c8b43be4e5590d7e086bfe64c89
Annika <makelannika@gmail.com>
2025-06-14 01:14:39 +0300

    displaying friends list and their online status for logged in user

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -1,42 +1,79 @@
-import React, { useRef, useState } from "react"
+import { useState, useEffect } from "react"
 import { useTranslation } from "react-i18next"
 
 import { useUser } from "../contexts/UserContext"
-import PieCharts from "../components/PieCharts"
-import AreaCharts from "../components/AreaCharts"
+import type { User} from "../contexts/UserContext"
+import { ApiClient } from "@transcenders/api-client"
+// import PieCharts from "../components/PieCharts"
+// import AreaCharts from "../components/AreaCharts"
 import MatchHistory from "../components/MatchHistory"
 
 const Dashboard = () => {
   const { t } = useTranslation()
   const { user } = useUser()
 
-  const [avatar, setAvatar] = useState<File | null>(null)
-  const [previewUrl, setPreviewUrl] = useState<string | null>(null)
-  const fileInputRef = useRef<HTMLInputElement>(null)
+	const [avatarIdx, setAvatarIdx] = useState<number>(0)
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (file) {
-      setAvatar(file);
-      setPreviewUrl(URL.createObjectURL(file));
-    }
-  };
+	const avatarSizes = [
+		"max-w-[65%]",
+		"max-w-[85%]",
+		"max-w-[98%]",
+		"max-w-[80%]",
+		"max-w-[80%]",
+		"max-w-[70%]",
+	]
 
-  const handleUploadClick = () => {
-    fileInputRef.current?.click()
-  }
+	const friends = [
+		{ id: 1, name: "Mittens", online: true },
+		{ id: 2, name: "Shadow", online: false },
+		{ id: 3, name: "Whiskers", online: true },
+		{ id: 4, name: "Mittens", online: true },
+		{ id: 5, name: "Shadow", online: false },
+		{ id: 6, name: "Whiskers", online: true },
+		{ id: 7, name: "Mittens", online: true },
+		{ id: 8, name: "Shadow", online: false },
+		{ id: 9, name: "Whiskers", online: true },
+		{ id: 10, name: "Mittens", online: true },
+		{ id: 11, name: "Shadow", online: false },
+		{ id: 12, name: "Whiskers", online: true },
+		{ id: 13, name: "Mittens", online: true },
+		{ id: 14, name: "Shadow", online: false },
+		{ id: 15, name: "Whiskers", online: true },
+	]
 
-  const wins = 42
-  const losses = 13
+	const [viewedUser, setViewedUser] = useState<User | null>(user)
+	const [searchId, setSearchId] = useState("")
+	const [error, setError] = useState("")
 
-  return (
-		<div className="w-full h-full flex gap-6">
-			<div className="profile-box basis-1/5 justify-between p-6 py-16">
+	const handleSearch = async () => {
+		setError("")
+		const id = parseInt(searchId)
+
+		try {
+			const response = await ApiClient.user.getUserById(id)
+			
+			if (response?.success && response?.data) {
+				setViewedUser(response.data as User)
+			} else {
+				setError("User not found")
+			}
+
+		} catch (err: any) {
+			setError(err.message || t('something_went_wrong'))
+		}
+	}
+
+	const wins = 42
+	const losses = 13
+
+  	return (
+		<div className="w-full h-full flex gap-10">
+			<div className="profile-box rounded-lg basis-1/5 px-24 py-16">
 				
-				<div className="flex flex-col items-center">
-					{previewUrl ? (
-					<img
-						src={previewUrl}
+				<div className="flex basis-4/12 flex-col items-center">
+					<div className="bubble bg-opacity-50 w-64 h-64 flex items-end justify-center overflow-hidden">
+						<img
+						src={avatars[avatarIdx]}
 						alt="Avatar preview"
 						className="bubble w-64 object-cover"
 						/>
@@ -44,44 +81,48 @@ const Dashboard = () => {
 					<div className="bubble w-64 bg-white bg-opacity-25"></div>
 					)}
 					
-					<h1 className="pt-6 text-3xl uppercase">{user?.name}</h1>
-
-					<input
-						type="file"
-						accept="image/*"
-						onChange={handleFileChange}
-						ref={fileInputRef}
-						className="hidden"
-						/>
-
-					<button
-						onClick={handleUploadClick}
-						className="text-[#ffebe7b7] hover:text-white py-2"
-						>
-						{t("avatar")}
-					</button>
-
-					<div className="flex flex-col pt-16">
-						<button className="play-button min-w-36 m-2">
-							Add Friend
-						</button>
-
-						<button className="play-button min-w-36 m-2">
-							Remove Friend
-						</button>
-						
-						<button className="play-button min-w-36 m-2">
-							Block User
-						</button>
-					</div>
+					<h1 className="pt-6 text-5xl text-[#fff] font-fascinate">{viewedUser?.username}</h1>
 				</div>
 
-				<div className="flex w-full justify-center items-center">
-					<p className="text-lg pr-4">Search User:</p>
+					{user?.id === viewedUser?.id ? (
+						<>
+						<div className="flex basis-7/12 w-full flex-col items-center justify-center">
+							<div className="w-full h-[85%] bg-white/20 rounded-full border border-white p-10 custom-scrollbar">
+								<p className="pb-2 text-white text-center font-fascinate uppercase text-xl mb-4">Friends</p>
+								<div className="relative h-[80%] p-2 overflow-y-auto">
+									{friends.map((friend) => (
+										<div key={friend.id} className="flex items-center justify-between text-white py-1">
+											<span>{friend.name}</span>
+											<span
+												className={`w-3 h-3 rounded-full ${
+													friend.online ? 'bg-green-300' : 'bg-gray-200'
+												}`}
+											></span>
+										</div>
+									))}
+								</div>
+							</div>
+						</div>
+						</>
+					) : (
+						<div className="flex basis-7/12 w-full flex-col items-center">
+							<div className="flex flex-col pt-16">
+								<button className="play-button min-w-36 m-2">{t('add_friend')}</button>
+								<button className="play-button min-w-36 m-2">{t('remove_friend')}</button>
+								<button className="play-button min-w-36 m-2">{t('block_user')}</button>
+							</div>
+						</div>
+					)}
+	
+				<div className="flex basis-1/12 w-full justify-center items-end">
 					<input
 						type="text"
-						className="login-input-field"
-				/>
+						value={searchId}
+						placeholder={t('username')}
+						onChange={(e) => setSearchId(e.target.value)}
+						className="login-input-field min-w-24 p-2"
+					/>
+					<button onClick={handleSearch} className="ml-4 p-2">Search</button>
 				</div>
 			</div>
 			
