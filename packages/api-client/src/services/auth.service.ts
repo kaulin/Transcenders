7ff53277fcf5409e4bcca38ef1b82e3e204a0413
aklein <allarklein@gmail.com>
2025-06-15 03:29:27 +0300

    feat: added register and login functions through auth-service to the api-client package

--- packages/api-client/src/services/auth.service.ts
+++ packages/api-client/src/services/auth.service.ts
@@ -1,4 +1,10 @@
-import { ApiResponseType, AUTH_ROUTES, SERVICE_URLS } from '@transcenders/contracts';
+import {
+  ApiResponseType,
+  AUTH_ROUTES,
+  LoginUser,
+  RegisterUser,
+  SERVICE_URLS,
+} from '@transcenders/contracts';
 import { ApiClient } from '../api/ApiClient';
 import { ApiCallOptions } from '../types/client.options';
 
@@ -16,14 +22,24 @@ export class AuthApiService {
 
   /**
    * Gets a list of users based on optional query parameters
-   * #TODO register() input/request type
+   * TODO register() input/request type
    */
 
-  static async register(username: string, password: string) {
-    const endpoint = `${AUTH_ROUTES.AUTH.replace(':username', username).replace(
-      ':password',
-      password,
-    )}`;
-    return this.callAUthService(endpoint);
+  static async register(registration: RegisterUser) {
+    const endpoint = `${AUTH_ROUTES.REGISTER}`;
+    const options: ApiCallOptions = {
+      method: 'POST',
+      body: registration,
+    };
+    return this.callAUthService(endpoint, options);
+  }
+
+  static async login(login: LoginUser) {
+    const endpoint = `${AUTH_ROUTES.LOGIN}`;
+    const options: ApiCallOptions = {
+      method: 'POST',
+      body: login,
+    };
+    return this.callAUthService(endpoint, options);
   }
 }
