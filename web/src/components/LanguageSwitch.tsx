9897987fd7f80f2469c945ae237107a93b0577b1
Annika <makelannika@gmail.com>
2025-06-16 00:11:13 +0300

    minor changes

--- web/src/components/LanguageSwitch.tsx
+++ web/src/components/LanguageSwitch.tsx
@@ -8,9 +8,9 @@ function LanguageSwitch () {
     }
 
     return (
-        <div className="flex gap-2">
+        <div className="flex gap-2 text-lg">
             <button onClick={() => handleLanguageChange('en')}>EN</button>
-            <>|</>
+            |
             <button onClick={() => handleLanguageChange('fi')}>FI</button>
         </div>
     )
