e1ac7a06f64851b3ebcab4cbf459477fa03601e3
Aida Neitenbach <aidaolivia404@gmail.com>
2025-06-16 18:32:16 +0300

    start of 1v1 and tournament context files, still have to input most functionality

---
+++ web/src/contexts/OnevOneContext.tsx
@@ -0,0 +1,12 @@
+export type OnevOne = {
+	player1: {
+		id: number;
+		username: string;
+		avatar: string;
+	};
+	player2: {
+		id: number;
+		username: string;
+		avatar: string;
+	};
+}
---
+++ web/src/contexts/TournamentContext.tsx
@@ -0,0 +1,29 @@
+export type TournamentContext = {
+	tournamentId: number;
+	players: {
+		player1: {id:number; name: string; avatar:string; };
+		player2: {id:number; name:string; avatar:string; };
+		player3: {id:number; name:string; avatar:string; };
+		player4: {id:number; name:string; avatar:string; };
+	}
+	bracket: {
+		round1:{
+			gameId: number;
+			player1Key: 'player1' | 'player2' | 'player3' | 'player4';
+			player2Key: 'player1' | 'player2' | 'player3' | 'player4';
+			winner?: 'player1Key' | 'player2Key';
+		};
+		round2:{
+			gameId: number;
+			player1Key: 'player1' | 'player2' | 'player3' | 'player4';
+			player2Key: 'player1' | 'player2' | 'player3' | 'player4';
+			winner?: 'player1Key' | 'player2Key';
+		};
+		round3:{
+			gameId: number;
+			player1Key: 'player1' | 'player2' | 'player3' | 'player4';
+			player2Key: 'player1' | 'player2' | 'player3' | 'player4';
+			winner?: 'player1Key' | 'player2Key';
+		};
+	}
+}
