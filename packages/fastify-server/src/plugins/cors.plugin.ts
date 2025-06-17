5c4dc91725785a92e74fbbc774161924fb5ae06b
aklein <aklein@student.hive.fi>
2025-06-18 01:25:57 +0300

    fix: another CORS issue fixed, default fastify didnt allow PATCH, PUT or DELETE from different origins that require CORS

--- packages/fastify-server/src/plugins/cors.plugin.ts
+++ packages/fastify-server/src/plugins/cors.plugin.ts
@@ -5,5 +5,7 @@ export async function registerCors(fastify: FastifyInstance) {
   await fastify.register(cors, {
     origin: [/^http:\/\/localhost:\d+$/, /^http:\/\/127\.0\.0\.1:\d+$/],
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
 }
