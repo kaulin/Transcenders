1383addeae11b9d4580d9c48624ad8bbdb2d1e70
Aida Neitenbach <aneitenb@student.hive.fi>
2025-06-17 15:54:33 +0300

    implementing 1v1 Context into GameContainer and Canvas

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -5,12 +5,15 @@ import App from './App.tsx'
 import './styles/index.css'
 import './i18n/i18n'
 import { UserProvider } from './contexts/UserContext.tsx'
+import { OneVOneProvider } from './contexts/OneVOneContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <OneVOneProvider>
+          <App />
+        </OneVOneProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
