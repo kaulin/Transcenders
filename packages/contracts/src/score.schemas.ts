8a819f26e727911ff14aa0c116403b873de55123
Jaakko Junttila <jajuntti@student.hive.fi>
2025-06-18 16:57:52 +0300

    Removes redundant exports from score schema

--- packages/contracts/src/score.schemas.ts
+++ packages/contracts/src/score.schemas.ts
@@ -1,40 +1,18 @@
 import { Static, Type } from '@sinclair/typebox';
-export const WinnerIdField = Type.Number();
-export const LoserIdField = Type.Number();
-export const WinnerScoreField = Type.Number();
-export const LoserScoreField = Type.Number();
-export const TournamentLevelField = Type.Number();
-export const GameDurationField = Type.Number();
-export const GameStartTimeField = Type.Number();
-export const GameEndTimeField = Type.Number();
-export const ScoresByIdField = Type.Number();
-export const TotalGamesField = Type.Number();
-export const TotalWinsField = Type.Number();
-export const TotalWinPercentageField = Type.Number();
-export const RegularGamesField = Type.Number();
-export const RegularGameWinsField = Type.Number();
-export const RegularGameWinPercentageField = Type.Number();
-export const TournamentGamesField = Type.Number();
-export const TournamentGameWinsField = Type.Number();
-export const TournamentGameWinPercentageField = Type.Number();
-export const TournamentWinsField = Type.Number();
-export const TotalScoreField = Type.Number();
-export const AverageScoreField = Type.Number();
-export const TotalDurationField = Type.Number();
-export const AverageDurationField = Type.Number();
+import { TimestampField, UserIdField } from './user.schemas';
 
 /**
  * ENTITY SCHEMAS
  */
 export const ScoreSchema = Type.Object({
-  winner_id: WinnerIdField,
-  loser_id: LoserIdField,
-  winner_score: WinnerScoreField,
-  loser_score: LoserScoreField,
-  tournament_level: TournamentLevelField,
-  game_duration: GameDurationField,
-  game_start: GameStartTimeField,
-  game_end: GameEndTimeField,
+  winner_id: UserIdField,
+  loser_id: UserIdField,
+  winner_score: Type.Number(),
+  loser_score: Type.Number(),
+  tournament_level: Type.Number(),
+  game_duration: Type.Number(),
+  game_start: TimestampField,
+  game_end: TimestampField,
 });
 export type Score = Static<typeof ScoreSchema>;
 
@@ -42,20 +20,20 @@ export const ScoresArraySchema = Type.Array(ScoreSchema);
 export type ScoresArray = Static<typeof ScoresArraySchema>;
 
 export const StatsSchema = Type.Object({
-  total_games: TotalGamesField,
-  total_wins: TotalWinsField,
-  total_win_percentage: TotalWinPercentageField,
-  regular_games: RegularGamesField,
-  regular_game_wins: RegularGameWinsField,
-  regular_game_win_percentage: RegularGameWinPercentageField,
-  tournament_games: TournamentGamesField,
-  tourament_game_wins: TournamentWinsField,
-  tournament_game_win_percentage: TournamentGameWinPercentageField,
-  tournament_wins: TournamentWinsField,
-  total_score: TotalScoreField,
-  average_score: AverageScoreField,
-  total_duration: TotalDurationField,
-  average_duration: AverageDurationField
+  total_games: Type.Number(),
+  total_wins: Type.Number(),
+  total_win_percentage: Type.Number(),
+  regular_games: Type.Number(),
+  regular_game_wins: Type.Number(),
+  regular_game_win_percentage: Type.Number(),
+  tournament_games: Type.Number(),
+  tourament_game_wins: Type.Number(),
+  tournament_game_win_percentage: Type.Number(),
+  tournament_wins: Type.Number(),
+  total_score: Type.Number(),
+  average_score: Type.Number(),
+  total_duration: Type.Number(),
+  average_duration: Type.Number(),
 });
 export type Stats = Static<typeof StatsSchema>;
 
@@ -64,14 +42,14 @@ export type Stats = Static<typeof StatsSchema>;
  */
 export const createScoreSchema = {
   body: Type.Object({
-    winner_id: WinnerIdField,
-    loser_id: LoserIdField,
-    winner_score: WinnerScoreField,
-    loser_score: LoserScoreField,
-    tournament_level: TournamentLevelField,
-    game_duration: GameDurationField,
-    game_start: GameStartTimeField,
-    game_end: GameEndTimeField,
+    winner_id: UserIdField,
+    loser_id: UserIdField,
+    winner_score: Type.Number(),
+    loser_score: Type.Number(),
+    tournament_level: Type.Number(),
+    game_duration: Type.Number(),
+    game_start: TimestampField,
+    game_end: TimestampField,
   }),
 };
 export type CreateScoreRequest = Static<typeof createScoreSchema.body>;
@@ -101,14 +79,14 @@ export type GetScoreRequest = Static<typeof getScoreSchema.querystring>;
 
 export const scoresByIdSchema = {
   params: Type.Object({
-    id: ScoresByIdField,
+    id: UserIdField,
   }),
 };
 export type ScoresByIdRequest = Static<typeof scoresByIdSchema.params>;
 
 export const statsByIdSchema = {
   params: Type.Object({
-    id: ScoresByIdField,
+    id: UserIdField,
   }),
 };
 export type StatsByIdRequest = Static<typeof statsByIdSchema.params>;
