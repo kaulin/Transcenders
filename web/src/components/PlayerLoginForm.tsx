bd510800c0975955364602b67dc4a29c2037e416
makelannika <makelannika@gmail.com>
2025-06-18 21:51:00 +0300

    modified player validation system to use PlayersContext, fixed translations and added username checks

--- web/src/components/PlayerLoginForm.tsx
+++ web/src/components/PlayerLoginForm.tsx
@@ -1,41 +1,73 @@
 import { useState } from "react"
+import { useTranslation } from "react-i18next"
+
 import useVerifyLogin from "../hooks/useVerifyLogin.ts"
+import { usePlayers } from "../contexts/PlayersContext.tsx"
+import { ApiClient } from "@transcenders/api-client"
 import type { Player } from "../types/types.ts"
 
 type Props = {
   playerNumber: number;
   player?: Player;
-  setPlayer: (playerData: Player) => void;
 }
 
-const PlayerLoginForm = ({ playerNumber, player, setPlayer }: Props) => {
+const PlayerLoginForm = ({ playerNumber, player }: Props) => {
+	const { players, setPlayer, resetPlayer } = usePlayers()
 	const { login } = useVerifyLogin()
+	const { t } = useTranslation()
 
-	const [playerMode, setPlayerMode] = useState<Player["mode"] | null>(null)
-	const [displayName, setDisplayName] = useState<string>("")
 	const [username, setUsername] = useState<string>("")
 	const [password, setPassword] = useState<string>("")
 	const [error, setError] = useState<string | null>(null)
 
-	const handleGuestContinue = () => {
-		if (!displayName?.trim()) {
-			setError("Please input username")
+	const handleGoBack = () => {
+		resetPlayer(playerNumber)
+		setUsername("")
+		setPassword("")
+		setError(null)
+	}
+
+	const handleUsernameCheck = () => {
+		const taken = Object.values(players).some(p => p.username === username)
+		if (taken) {
+			setError(t('username_taken'))
+			return false
+		}
+		return true
+	}
+
+	const handleGuestContinue = async () => {
+		if (!username?.trim()) {
+			setError(t('input_username'))
 			return
 		}
 
-		setPlayer({
-			id: undefined,
-			username: displayName?.trim(),
-			mode: "guest",
+		if (!handleUsernameCheck())
+			return
+		
+		try {
+			const response = await ApiClient.user.getUserExact({ username: username })
+			if (response?.success) {
+				setError(t('username_taken'))
+				return
+			}
+		} catch {}
+
+		setPlayer(playerNumber, {
+			id: 0,
+			username: username,
 			ready: true
 		})
 	}
 
 	const handleLoginContinue = async () => {
 		if (!username?.trim() || !password) {
-			setError("Please input username and password")
+			setError(t('input_username_pw'))
 			return
 		}
+
+		if (!handleUsernameCheck())
+			return
 
 		try {
 			const verifiedUser = await login(username?.trim(), password)
@@ -43,41 +75,55 @@ const PlayerLoginForm = ({ playerNumber, player, setPlayer }: Props) => {
 				return
 			}
 			
-			setPlayer({
+			setPlayer(playerNumber, {
 				id: verifiedUser.id,
 				username: verifiedUser.username,
 				avatar: verifiedUser.avatar,
-				mode: "login",
 				ready: true
 			})
 
 		} catch (err: any) {
-			setError(err.message)
+			setError(t('incorrect_username_pw'))
 		}
 	}
 
-	const handleGoBack = () => {
-		setPlayerMode(null)
-		setDisplayName("")
-		setUsername("")
-		setPassword("")
-		
-		setPlayer({
-			id: undefined,
-			username: "",
-			mode: null,
-			ready: false,
-		} as Player)
+	const backAndContinueButtons = (onContinue: () => void) => (
+		<div className="flex justify-between">
+			<button className="mt-2 p-2" onClick={handleGoBack}>
+				← {t('go_back')}
+			</button>
+
+			<button className="mt-2 p-2" onClick={onContinue}>
+				{t('continue')} ➜
+			</button>
+		</div>
+	)
+
+	if (!player?.mode) {
+		return (
+			<>
+				<h2 className="profile-label">{t('player')} {playerNumber}</h2>
+				<div className="flex gap-2 items-start text-sm">
+					<button className="profile-button mt-0" onClick={() => setPlayer(playerNumber, {mode: "guest"})}>
+						{t('play_as_guest')}
+					</button>
+
+					<button className="profile-button mt-0" onClick={() => setPlayer(playerNumber, {mode: "login"})}>
+						{t('log_in')}
+					</button>
+				</div>
+			</>
+		)
 	}
 
 	if (player?.ready) {
 		return (
 			<>
-				<h2 className="profile-label">Player {playerNumber}</h2>
+				<h2 className="profile-label">{t('player')} {playerNumber}</h2>
 				<p className="border-b-2 border-white text-lg">{player.username}</p>
 				<div className="flex justify-between">
 					<button className="mt-2 p-2" onClick={handleGoBack}>
-						← Go back
+						← {t('go_back')}
 					</button>
 
 					<p className="text-lg pt-4">✓</p>
@@ -87,73 +133,38 @@ const PlayerLoginForm = ({ playerNumber, player, setPlayer }: Props) => {
 	}
 
 	return (
-		<div className="mb-6">
-			<h2 className="profile-label">Player {playerNumber}</h2>
-			
-			{playerMode === null && (
-				<div className="flex gap-2 items-start text-sm">
-					<button className="profile-button mt-0" onClick={() => setPlayerMode("guest")}>
-						Play as Guest
-					</button>
+		<>
+			<h2 className="profile-label">{t('player')} {playerNumber}</h2>
+			<input
+				type="text"
+				placeholder={t('username')}
+				className="profile-input-field"
+				value={username}
+				onChange={(e) => setUsername(e.target.value)}
+			/>
 
-					<button className="profile-button mt-0" onClick={() => setPlayerMode("login")}>
-						Log in
-					</button>
+			{player?.mode === "login" &&
+				<input
+					type="password"
+					placeholder={t('password')}
+					className="profile-input-field"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>		
+			}
+
+			{player.mode === "login"
+				? backAndContinueButtons(handleLoginContinue)
+				: backAndContinueButtons(handleGuestContinue)
+			}
+
+			{error && (
+				<div className="text-[#513838]">
+					{error}
 				</div>
 			)}
-
-			{playerMode === "login" && (
-				<>
-					<input
-						type="text"
-						placeholder="Username"
-						className="profile-input-field"
-						value={username}
-						onChange={(e) => setUsername(e.target.value)}
-					/>
-
-					<input
-						type="password"
-						placeholder="Password"
-						className="profile-input-field"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-
-					<div className="flex justify-between">
-						<button className="mt-2 p-2" onClick={handleGoBack}>
-							← Go back
-						</button>
-
-						<button className="mt-2 p-2" onClick={handleLoginContinue}>
-							Continue ➜
-						</button>
-					</div>
-				</>
-			)}
-
-			{playerMode === "guest" && (
-				<>
-					<input
-						type="text"
-						placeholder="Display name"
-						className="profile-input-field"
-						value={displayName}
-						onChange={(e) => setDisplayName(e.target.value)}
-					/>
-					<div className="flex justify-between">
-						<button className="mt-2 p-2" onClick={handleGoBack}>
-							← Go back
-						</button>
-
-						<button className="mt-2 p-2" onClick={handleGuestContinue}>
-							Continue ➜
-						</button>
-					</div>
-				</>
-			)}
-		</div>
+		</>
 	)
-};
+}
 
 export default PlayerLoginForm
--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,19 +1,30 @@
+import { Link } from 'react-router-dom'
+import { useState, useEffect } from "react"
+
 import { useTranslation } from "react-i18next"
 import { useUser } from "../contexts/UserContext"
-import { Link } from 'react-router-dom'
-import { useState } from "react"
-import type { Player } from "../types/types.ts"
-
+import { usePlayers } from "../contexts/PlayersContext.tsx"
 import PlayerLoginForm from "../components/PlayerLoginForm"
-
 import curiousCat from "/images/curiousCat.avif"
 
 const Home = () => {
     const { t } = useTranslation()
     const { user } = useUser()
+	const { players, setPlayer } = usePlayers()
 
-	const [players, setPlayers] = useState<Record<number, Player>>({})
 	const [gameMode, setGameMode] = useState<"match" | "tournament" | null>(null)
+
+	useEffect(() => {
+		if (user?.id) {
+			setPlayer(1, {
+				id: user?.id,
+				username: user?.username,
+				avatar: user?.avatar,
+				mode: "login",
+				ready: true
+			})
+		}
+	}, [user?.id])
 
     return (
 		<div className="w-full h-full profile-box flex-row">
@@ -38,25 +49,23 @@ const Home = () => {
 				</div>
 
 			</div>
-			<div className="h-full flex flex-col basis-1/2 px-10 py-14 bg-[#605c4c13] items-center justify-center">
+
+			<div className="h-full flex flex-col basis-1/2 px-10 gap-2 bg-[#605c4c13] items-center justify-center">
 				{gameMode === "match" && (
 					<div className="w-full h-full max-w-sm flex flex-col">
 						<div className="flex basis-1/4 flex-col"></div>
 						<div className="flex basis-1/4 flex-col justify-end pb-8">
-							<h2 className="profile-label">Player 1</h2>
+							<h2 className="profile-label">{t('player')} 1</h2>
 							<p className="mb-6 border-b-2 border-white text-lg flex justify-between">{user?.username}</p>
 						</div>
 						<div className="flex basis-1/4 flex-col justify-start">
 							<PlayerLoginForm
 								playerNumber={2}
 								player={players[2]}
-								setPlayer={(playerData: Player) =>
-									setPlayers((prev) => ({ ...prev, [2]: playerData}))
-								}	
 							/>
 						</div>
 						<div className="flex basis-1/4 items-center justify-center">
-							{gameMode === "match" && players[2]?.ready && (
+							{players[2]?.ready && (
 								<Link to="/LocalGame" className="profile-button">start</Link>
 							)}
 						</div>
@@ -67,42 +76,30 @@ const Home = () => {
 					<div className="w-full h-full max-w-sm flex flex-col">
 						<div className="flex basis-1/6"></div>
 						<div className="flex basis-1/6 flex-col justify-center">
-							<h2 className="profile-label">Player 1</h2>
+							<h2 className="profile-label">{t('player')} 1</h2>
 							<p className="mb-6 border-b-2 border-white pb-1 text-lg">{user?.username}</p>
 						</div>
 						<div className="flex basis-1/6 flex-col justify-start">
 							<PlayerLoginForm
 								playerNumber={2}
 								player={players[2]}
-								setPlayer={(playerData: Player) =>
-									setPlayers((prev) => ({ ...prev, [2]: playerData}))
-								}
 							/>
 						</div>
 						<div className="flex basis-1/6 flex-col justify-start">
 							<PlayerLoginForm
 								playerNumber={3}
 								player={players[3]}
-								setPlayer={(playerData: Player) =>
-									setPlayers((prev) => ({ ...prev, [3]: playerData}))
-								}
 							/>
 						</div>
 						<div className="flex basis-1/6 flex-col justify-start">
 							<PlayerLoginForm
 								playerNumber={4}
 								player={players[4]}
-								setPlayer={(playerData: Player) =>
-									setPlayers((prev) => ({ ...prev, [4]: playerData}))
-								}
 							/>
 						</div>
 						<div className="flex basis-1/6 items-center justify-center">
-							{gameMode === "tournament" &&
-								players[2]?.ready &&
-								players[3]?.ready &&
-								players[4]?.ready && (
-									<Link to="/LocalGame" className="profile-button mt-0">start</Link>
+							{players[2]?.ready && players[3]?.ready && players[4]?.ready && (
+								<Link to="/LocalGame" className="profile-button mt-0">start</Link>
 							)}
 						</div>
 					</div>
