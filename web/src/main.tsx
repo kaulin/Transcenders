d27049612bc184b56b35fd630a51eb0d7f3e559f
makelannika <makelannika@gmail.com>
2025-06-18 21:46:47 +0300

    included PlayersProvider

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -5,15 +5,15 @@ import App from './App.tsx'
 import './styles/index.css'
 import './i18n/i18n'
 import { UserProvider } from './contexts/UserContext.tsx'
-import { OneVOneProvider } from './contexts/OneVOneContext.tsx'
+import { PlayersProvider } from './contexts/PlayersContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <OneVOneProvider>
+        <PlayersProvider>
           <App />
-        </OneVOneProvider>
+        </PlayersProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
