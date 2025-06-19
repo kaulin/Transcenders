dcb20ec4e57b60fb8d6f152792218dc2ccebcabf
Jaakko Junttila <jajuntti@student.hive.fi>
2025-06-19 15:55:16 +0300

    Adds base files for gateway-service

---
+++ services/gateway-service/src/controllers/GatewayController.ts
@@ -0,0 +1,9 @@
+import {
+  ResponseHelper,
+} from '@transcenders/contracts';
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { ScoreService } from '../services/GatewayService';
+
+export class GatewayController {
+
+}
