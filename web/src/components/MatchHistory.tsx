149419d54b4a3a9106b24fedcd33e6517ee3bc54
Annika <makelannika@gmail.com>
2025-06-24 01:03:25 +0300

    layout changes for better responsiveness

--- web/src/components/MatchHistory.tsx
+++ web/src/components/MatchHistory.tsx
@@ -14,7 +14,7 @@ const MatchHistory = () => {
 		{ outcome: "-", p1: "-", p2: "-", date: "-" },
 		{ outcome: "-", p1: "-", p2: "-", date: "-" },
 	  ].map(({ outcome, p1, p2, date }) => (
-		<div className="flex bg-[#605c4c13] border rounded-full p-4 justify-around items-center">
+		<div className="flex bg-[#605c4c13] border rounded-full min-w-[400px] p-4 justify-around items-center">
 			<div className="text-[#fff]">{outcome}</div>
 			<div className="text-[#fff]">{p1} - {p2}</div>
 			<div className="text-[#fff]">{date}</div>
