790ad532833dc8650611e77a1b3a93f91b2af5ae
Annika <makelannika@gmail.com>
2025-06-24 01:03:25 +0300

    layout changes for better responsiveness

--- web/src/components/PieCharts.tsx
+++ web/src/components/PieCharts.tsx
@@ -16,48 +16,50 @@ const PieCharts = () => {
 
   const matchTypeData = [
     { name: 'Tournament', value: 20 },
-    { name: 'Single Match', value: 35 },
+    { name: 'Match', value: 35 },
   ]; 
 
   return (
-    <div className="flex w-full">
+    <div className="flex flex-col gap-12 md:flex-row md:gap-0 w-full">
       {[
         { title: "Matches by Type", data: matchTypeData },
         { title: "Single Match", data: singleMatchData },
         { title: "Tournament", data: tournamentData },
       ].map(({ title, data }, i) => (
-        <div key={i} className="flex flex-col items-center min-w-[200px] flex-1">
-          <div className="text-white mb-2">{title}</div>
-          <div className="w-full h-52">
-            <ResponsiveContainer width="100%" height="100%">
-              <PieChart>
-                <Pie
-                  data={data}
-                  dataKey="value"
-                  nameKey="name"
-                  cx="50%"
-                  cy="50%"
-                  innerRadius={40}
-                  outerRadius={80}
-                >
-                  <Cell fill={COLORS[0]} />
-                  <Cell fill={COLORS[1]} />
-                </Pie>
-                <Tooltip
-                  contentStyle={{ backgroundColor: '#333', border: 'none', color: '#fff' }}
-                  labelStyle={{ color: '#fff' }}
-                  itemStyle={{ color: '#fff' }}
-                />
-                <Legend
-                  formatter={(value) => <span style={{ color: '#fff' }}>{value}</span>}
-                />
-              </PieChart>
-            </ResponsiveContainer>
+        <div key={i} className="flex flex-col flex-1 items-center justify-center min-w-[200px]">
+          <div>
+            <div className="text-white text-center">{title}</div>
+            <div className="w-52 h-52">
+              <ResponsiveContainer width="100%" height="100%">
+                <PieChart>
+                  <Pie
+                    data={data}
+                    dataKey="value"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                    innerRadius={40}
+                    outerRadius={80}
+                  >
+                    <Cell fill={COLORS[0]} />
+                    <Cell fill={COLORS[1]} />
+                  </Pie>
+                  <Tooltip
+                    contentStyle={{ backgroundColor: '#333', border: 'none', color: '#fff' }}
+                    labelStyle={{ color: '#fff' }}
+                    itemStyle={{ color: '#fff' }}
+                  />
+                  <Legend
+                    formatter={(value) => <span style={{ color: '#fff' }}>{value}</span>}
+                  />
+                </PieChart>
+              </ResponsiveContainer>
+            </div>
           </div>
         </div>
       ))}
     </div>
-  );
-};
+  )
+}
 
 export default PieCharts
--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next"
 import { useUser } from "../contexts/UserContext"
 import type { User} from "@transcenders/contracts"
 import { ApiClient } from "@transcenders/api-client"
-// import PieCharts from "../components/PieCharts"
+import PieCharts from "../components/PieCharts"
 import AreaCharts from "../components/AreaCharts"
 import MatchHistory from "../components/MatchHistory"
 
@@ -73,9 +73,9 @@ const Dashboard = () => {
 	const losses = 13
 
   	return (
-		<div className="w-full h-full flex gap-10">
-			<div className="profile-box rounded-lg basis-1/5 px-24 py-16">
-				
+		<div className="profile-box">
+			{/* profile */}
+			<div className="flex lg:basis-1/4 w-full h-full flex-col flex-shrink-0 bg-[#605c4c13] items-center px-8 py-16">
 				<div className="flex basis-4/12 flex-col items-center">
 					<div className="bubble bg-white/50 w-64 h-64 flex items-end justify-center overflow-hidden">
 						<img
@@ -84,14 +84,14 @@ const Dashboard = () => {
 						className={`object-contain ${avatarSizes[0]}`}
 						/>
 					</div>
-					
+
 					<h1 className="pt-6 text-5xl text-[#fff] font-fascinate">{viewedUser?.username}</h1>
 				</div>
 
-					{user?.id === viewedUser?.id ? (
-						<>
+	 			{user?.id === viewedUser?.id ? (
+					<>
 						<div className="flex basis-7/12 w-full flex-col items-center justify-center">
-							<div className="w-full h-[85%] bg-[#605c4c13] rounded-full border border-white p-10 custom-scrollbar">
+							<div className="w-80 h-[85%] bg-[#605c4c13] rounded-full border border-white p-10 custom-scrollbar">
 								<p className="pb-2 text-[#fff] text-center font-fascinate uppercase text-xl mb-4">Friends</p>
 								<div className="relative h-[80%] p-2 overflow-y-auto">
 									{friends.map((friend) => (
@@ -107,19 +107,19 @@ const Dashboard = () => {
 								</div>
 							</div>
 						</div>
-						</>
-					) : (
-						<div className="flex basis-7/12 w-full flex-col items-center">
-							<div className="flex flex-col pt-16">
-								<button className="play-button min-w-36 m-2">{t('add_friend')}</button>
-								<button className="play-button min-w-36 m-2">{t('remove_friend')}</button>
-								<button className="play-button min-w-36 m-2">{t('block_user')}</button>
-							</div>
+					</>
+				) : (
+					<div className="flex basis-7/12 w-full flex-col items-center">
+						<div className="flex flex-col pt-16">
+							<button className="play-button min-w-36 m-2">{t('add_friend')}</button>
+							<button className="play-button min-w-36 m-2">{t('remove_friend')}</button>
+							<button className="play-button min-w-36 m-2">{t('block_user')}</button>
 						</div>
-					)}
-				
-				<div className="flex basis-1/12 w-full flex-col items-start">
-					<div className="flex justify-center items-start">
+					</div>
+				)}
+
+				<div className="flex basis-1/12 w-full flex-col">
+					<div className="flex justify-center">
 						<input
 							type="text"
 							value={searchId}
@@ -127,49 +127,51 @@ const Dashboard = () => {
 							onChange={(e) => setSearchId(e.target.value)}
 							className="login-input-field min-w-24 p-2"
 							/>
-
 						<button onClick={handleSearch} className="ml-4 p-2">Search</button>
 					</div>
-
 					{error && (
 						<p className="text-[#513838] text-center pt-2">{error}</p>
 					)}
 				</div>
-			</div>
-			
-			<div className="profile-box basis-3/5 gap-6 p-6">
-				<div className="flex basis-1/5 w-full font-fascinate justify-around items-center text-lg uppercase">
-					<p className="text-4xl text-[#fff]">{t('games_played')} </p>
-				
-					<div className="bubble w-40 bg-[#605c4c13] border border-white flex flex-col justify-center items-center">
-						<div className="text-[#fff]">{t('total')}</div>
-						<div className="text-center text-[#fff]">{wins + losses}</div>
-					</div>
-					<div className="bubble w-40 bg-[#605c4c13] border border-white flex flex-col justify-center items-center">
-						<div className="text-[#fff]">{t('wins')}</div>
-						<div className="text-center text-[#fff]">{wins}</div>
-					</div>
-					<div className="bubble w-40 bg-[#605c4c13] border border-white flex flex-col justify-center items-center">
-						<div className="text-[#fff]">{t('losses')}</div>
-						<div className="text-center text-[#fff]">{losses}</div>
-					</div>
-				</div>
 
-				<div className="flex basis-2/5 items-center w-full">
-					{/* <PieCharts /> */}
-				</div>
-
-				<div className="flex basis-2/5 items-end w-full">
-					<AreaCharts />
-				</div>
 			</div>
 
-			<div className="profile-box basis-1/5 p-6 pt-28">
-				<h1 className="flex text-4xl text-[#fff] font-fascinate uppercase mb-16">
+			{/* stats */}
+			<div className="flex lg:flex-1 w-full min-h-full flex-col gap-28 flex-shrink-0 px-8 py-16">
+		 		<div className="flex w-full justify-center text-center items-center text-lg font-fascinate uppercase">
+		 			<p className="text-4xl text-[#fff]">{t('games_played')} </p>
+
+		 		</div>		
+		 		<div className="flex flex-col md:flex-row items-center justify-center gap-12 text-xl uppercase font-fascinate">
+		 			<div className="bubble min-w-40 bg-[#605c4c13] border border-white flex flex-col justify-center items-center">
+		 				<div className="text-[#fff]">{t('total')}</div>
+		 				<div className="text-center text-[#fff]">{wins + losses}</div>
+		 			</div>
+		 			<div className="bubble min-w-40 bg-[#605c4c13] border border-white flex flex-col justify-center items-center">
+		 				<div className="text-[#fff]">{t('wins')}</div>
+		 				<div className="text-center text-[#fff]">{wins}</div>
+		 			</div>
+		 			<div className="bubble min-w-40 bg-[#605c4c13] border border-white flex flex-col justify-center items-center">
+		 				<div className="text-[#fff]">{t('losses')}</div>
+		 				<div className="text-center text-[#fff]">{losses}</div>
+		 			</div>
+		 		</div>
+				 <div className="flex items-center justify-center gap-10">
+		 			<PieCharts />
+				</div>	
+		 		<div className="flex items-end w-full">
+		 			<AreaCharts />
+		 		</div>
+			</div>
+
+			{/* match history */}
+			<div className="flex lg:flex-1 flex-col flex-shrink-0 w-full h-full bg-[#605c4c13] px-8 py-16">
+				<h1 className="flex justify-center text-center text-4xl text-[#fff] font-fascinate uppercase mb-16">
 					{t('history')}
 				</h1>
 				<MatchHistory />
 			</div>
+
 		</div>
   )
 }
