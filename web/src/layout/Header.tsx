8055384a90164961caef17fc5696d3ad19158473
Annika <makelannika@gmail.com>
2025-06-24 01:04:22 +0300

    layout changes for better responsiveness

--- web/src/layout/Header.tsx
+++ web/src/layout/Header.tsx
@@ -18,28 +18,26 @@ const Header = () => {
 
     return (
         <header className="fixed top-0 left-0 w-full z-50 h-24">
-
-        { user? (
-            <>
-                <nav className="flex items-center py-9 px-10">
-					<div className="flex basis-1/2 justify-start gap-10">
-                    	<LanguageSwitch />
+            <nav className="flex items-center py-9 px-10 gap-10">
+            { user? (
+                <>
+                    <div className="flex basis-1/2 justify-start gap-10">
+                        <LanguageSwitch />
                         <div className="flex justify-center gap-4 uppercase">
                             <Link to="/" className="button-text text-lg">{t('home')}</Link>
                             <Link to="/dashboard" className="button-text text-lg">dashboard</Link>
                             <Link to="/profile" className="button-text text-lg">{t('profile')}</Link>
                         </div>
                     </div>
-					<div className="flex basis-1/2 justify-end text-lg">
-                    	<button onClick={handleLogout}>{t('log_out')}</button>
-					</div>
-                </nav>
-            </>
-        ) : (
-            <nav className="py-9 px-10">
+                    
+                    <div className="flex flex-nowrap basis-1/2 justify-end text-lg">
+                        <button onClick={handleLogout}>{t('log_out')}</button>
+                    </div>
+                </>
+            ) : (
                 <LanguageSwitch />
+            )}
             </nav>
-        )}
         </header>
     )
 }
