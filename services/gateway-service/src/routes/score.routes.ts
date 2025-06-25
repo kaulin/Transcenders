f5a5d5ffa9c1b659fb57de67c2461c2b8eca5d7e
Jaakko Junttila <jajuntti@student.hive.fi>
2025-06-25 20:03:04 +0300

    Adds gateway routes for users and scores

--- services/gateway-service/src/routes/score.routes.ts
+++ services/gateway-service/src/routes/score.routes.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance } from 'fastify';
-import { AuthController } from '../controllers/auth.controller';
+import { ScoreController } from '../controllers/score.controller';
 
-export default async function authRoutes(fastify: FastifyInstance) {
-
+export default async function scoreRoutes(fastify: FastifyInstance) {
+  fastify.get('/score', ScoreController.getScores);
+  fastify.post('/score', ScoreController.addScore);
+  fastify.get('/score/:id', ScoreController.getScoresById);
+  fastify.get('/score/:id/stats', ScoreController.getStatsById);
 }
--- services/gateway-service/src/routes/user.routes.ts
+++ services/gateway-service/src/routes/user.routes.ts
@@ -1,6 +1,21 @@
 import { FastifyInstance } from 'fastify';
-import { AuthController } from '../controllers/auth.controller';
+import { UserController } from '../controllers/user.controller';
 
-export default async function authRoutes(fastify: FastifyInstance) {
-
+export default async function userRoutes(fastify: FastifyInstance) {
+  // User routes
+  fastify.get('/users', UserController.getUsers);
+  fastify.post('/users', UserController.addUser);
+  fastify.get('/users/:id', UserController.getUserById);
+  fastify.patch('/users/:id', UserController.updateUser);
+  fastify.delete('/users/:id', UserController.deleteUser);
+  fastify.get('/users/:id/exists', UserController.checkUserExists);
+  fastify.get('users/exact', UserController.getUserExact);
+  // Friendship routes
+  fastify.get('/users/:id/friendships', UserController.getFriends);
+  fastify.delete('/users/:id/friendships/:friendId', UserController.removeFriend);
+  fastify.get('/users/:id/friend-requests', UserController.getRequests);
+  fastify.post('/users/:id/friend-requests/:recipientId', UserController.sendRequest);
+  fastify.put('/users/:id/friend-requests/:requestId', UserController.acceptFriend);
+  fastify.delete('/users/:id/friend-requests/:requestId', UserController.declineFriend);
+  fastify.get('/friendships/:id1/:id2', UserController.checkFriendshipExists);
 }
