e0c268fa642cfed903000f92d9be41397975b605
Annika <makelannika@gmail.com>
2025-07-09 00:10:39 +0300

    responsiveish dashboard

--- web/src/components/AreaCharts.tsx
+++ web/src/components/AreaCharts.tsx
@@ -6,58 +6,58 @@ import {
     CartesianGrid,
     Tooltip,
     ResponsiveContainer,
-    Legend,
   } from 'recharts'
   
   const GoalCharts = () => {
     const goalData = [
-      { match: 'Match 1', goalsScored: 11, goalsConceded: 2 },
-      { match: 'Match 2', goalsScored: 7, goalsConceded: 11 },
-      { match: 'Match 3', goalsScored: 11, goalsConceded: 6 },
-      { match: 'Match 4', goalsScored: 5, goalsConceded: 11 },
-      { match: 'Match 5', goalsScored: 11, goalsConceded: 9 },
+      { match: '', scored: 11, conceded: 2 },
+      { match: '', scored: 7, conceded: 11 },
+      { match: '', scored: 11, conceded: 6 },
+      { match: '', scored: 5, conceded: 11 },
+      { match: '', scored: 11, conceded: 9 },
     ]
   
     return (
-      <div className="flex w-full justify-center h-64">
-        <ResponsiveContainer width="90%" height="100%">
+      <div className="flex w-full justify-center items-center h-40 pr-10">
+        <ResponsiveContainer width="80%" height="80%">
           <AreaChart data={goalData}>
             <defs>
               <linearGradient id="scored" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="#def569" stopOpacity={0.8} />
-                <stop offset="95%" stopColor="#9fb52f" stopOpacity={0} />
+                <stop offset="5%" stopColor="#a7d4373c" stopOpacity={1} />
+                <stop offset="95%" stopColor="#fffb82" stopOpacity={0.2} />
+                {/* <stop offset="5%" stopColor="#ff8ba860" stopOpacity={1} />
+                <stop offset="95%" stopColor="#ff8bcb82" stopOpacity={0.2} /> */}
               </linearGradient>
               <linearGradient id="conceded" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="#e8d4c1" stopOpacity={0.8} />
-                <stop offset="95%" stopColor="#919191" stopOpacity={0} />
+                <stop offset="5%" stopColor="#5d6b2f52" stopOpacity={0.6} />
+                <stop offset="95%" stopColor="#5d6b2f52" stopOpacity={0} />
+                {/* <stop offset="5%" stopColor="#741c4d" stopOpacity={0.6} />
+                <stop offset="95%" stopColor="#741c4d" stopOpacity={0} /> */}
               </linearGradient>
             </defs>
             <CartesianGrid strokeDasharray="3 3" stroke="#ffffff33" />
-            <XAxis dataKey="match" stroke="#fff" />
+            <XAxis stroke="#fff" />
             <YAxis stroke="#fff" />
             <Tooltip 
               contentStyle={{ backgroundColor: '#333', border: 'none', color: '#fff' }}
               labelStyle={{ color: '#fff' }}
               itemStyle={{ color: '#fff' }}
             />
-            <Legend 
-              formatter={(value) => <span style={{ color: '#fff' }}>{value}</span>}
-            />
             <Area
               type="monotone"
-              dataKey="goalsConceded"
-              stroke="#b3a394"
+              dataKey="conceded"
+              stroke="#fff"
               fillOpacity={1}
               fill="url(#conceded)"
-              name="Goals Conceded"
+              name="Opponent"
             />
             <Area
               type="monotone"
-              dataKey="goalsScored"
-              stroke="#effcae"
+              dataKey="scored"
+              stroke="#fff"
               fillOpacity={1}
               fill="url(#scored)"
-              name="Goals Scored"
+              name="You"
             />
           </AreaChart>
         </ResponsiveContainer>
