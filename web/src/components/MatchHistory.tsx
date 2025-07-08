45369fe1eb31afe61a67a5e3559ab5d31dcd18d7
Annika <makelannika@gmail.com>
2025-07-09 00:10:39 +0300

    responsiveish dashboard

--- web/src/components/MatchHistory.tsx
+++ web/src/components/MatchHistory.tsx
@@ -1,28 +1,29 @@
 const MatchHistory = () => {
 
 	return (
-	  <div className="flex flex-col w-full h-full gap-8">
+		<div className="relative h-full w-full px-2 overflow-y-auto custom-scrollbar flex flex-col gap-12">
 		{[
 		  { outcome: "win", p1: "Player1", p2: "Player2", date: "24/05" },
 		  { outcome: "loss", p1: "Player1", p2: "Player2", date: "24/05" },
 		  { outcome: "win", p1: "Player1", p2: "Player2", date: "24/05" },
 		  { outcome: "loss", p1: "Player1", p2: "Player2", date: "24/05" },
 		  { outcome: "win", p1: "Player1", p2: "Player2", date: "24/05" },
-		  { outcome: "-", p1: "-", p2: "-", date: "-" },
-		  { outcome: "-", p1: "-", p2: "-", date: "-" },
-		  { outcome: "-", p1: "-", p2: "-", date: "-" },
-		  { outcome: "-", p1: "-", p2: "-", date: "-" },
-		  { outcome: "-", p1: "-", p2: "-", date: "-" },
+		  { outcome: "win", p1: "Player1", p2: "Player2", date: "24/05" },
+		  { outcome: "loss", p1: "Player1", p2: "Player2", date: "24/05" },
+		  { outcome: "win", p1: "Player1", p2: "Player2", date: "24/05" },
+		  { outcome: "loss", p1: "Player1", p2: "Player2", date: "24/05" },
+		  { outcome: "win", p1: "Player1", p2: "Player2", date: "24/05" },
 		].map(({ outcome, p1, p2, date }) => (
-		  <div className="flex flex-col md:flex-row md:min-w-[400px] bg-[#605c4c13] border rounded-full p-4 justify-around items-center">
-			  <div className="flex justify-center gap-6">
-				  <div className="text-[#fff] font-fascinate uppercase">{outcome}</div>
+		  <div className="flex flex-col justify-center items-center">
+			  <div className="flex justify-center items-center gap-6">
+				  <div className="text-[#fff] font-fascinate uppercase text-xl">{outcome}</div>
 				  <div className="text-[#fff]">{date}</div>
 			  </div>
-			  <div className="text-[#fff]">{p1} - {p2}</div>
+			  <div className="text-[#fff]">{p1}</div>
+			  <div className="text-[#fff]">{p2}</div>
 		  </div>
 		))}
-	  </div>
+		</div>
 	)
   }
   
--- web/src/components/PieCharts.tsx
+++ web/src/components/PieCharts.tsx
@@ -1,35 +1,51 @@
 import { PieChart, Pie, Cell, Tooltip, Legend, ResponsiveContainer } from 'recharts'
+import { useTranslation } from "react-i18next"
 
-const COLORS = ['#a8d43752', '#5d6b2f52']
+// const COLORS = ['#ffdcc4c3', '#6e5d414e']
+// const COLORS = ['#d6fb819a', '#6e5d414e']
+const COLORS = ['#a7d4373c', '#5d6b2f52']
+// const COLORS = ['#ff8ba860', '#741c4c82']
 
 const PieCharts = () => {
+  const { t } = useTranslation()
 
   const singleMatchData = [
-    { name: 'Wins', value: 30 },
-    { name: 'Losses', value: 5 },
+    { name: t('wins'), value: 30 },
+    { name: t('losses'), value: 5 },
   ];
 
   const tournamentData = [
-    { name: 'Wins', value: 12 },
-    { name: 'Losses', value: 8 },
+    { name: t('wins'), value: 12 },
+    { name: t('losses'), value: 8 },
   ];
 
   const matchTypeData = [
-    { name: 'Tournament', value: 20 },
-    { name: 'Match', value: 35 },
+    { name: t('tournament'), value: 20 },
+    { name: t('one_v_one'), value: 35 },
   ]; 
 
   return (
-    <div className="flex flex-col gap-12 md:flex-row md:gap-0 w-full">
+    <div className="flex flex-col w-full">
       {[
-        { title: "Matches by Type", data: matchTypeData },
-        { title: "Single Match", data: singleMatchData },
-        { title: "Tournament", data: tournamentData },
+        { title: t('mode'), data: matchTypeData },
+        { title: t('one_v_one'), data: singleMatchData },
+        { title: t('tournament'), data: tournamentData },
       ].map(({ title, data }, i) => (
-        <div key={i} className="flex flex-col flex-1 items-center justify-center min-w-[200px]">
-          <div>
-            <div className="text-white text-center">{title}</div>
-            <div className="w-60 h-52">
+        <div key={i} className="flex flex-col sm:flex-row w-full items-center justify-between">
+          <div className="flex flex-col items-center sm:items-start">
+            <div className="text-white text-center sm:text-xl font-fascinate uppercase">{title}</div>
+            {data.map((entry, index) => (
+              <div key={index} className="flex items-center gap-2 mb-1 text-md">
+              <span
+                className="inline-block w-4 h-4 rounded-full border border-white"
+                style={{ backgroundColor: COLORS[index % COLORS.length] }}
+              ></span>
+              <span>{entry.name}</span>
+            </div>
+            ))}
+          </div>
+
+            <div className="w-40 h-36">
               <ResponsiveContainer width="100%" height="100%">
                 <PieChart>
                   <Pie
@@ -38,24 +54,21 @@ const PieCharts = () => {
                     nameKey="name"
                     cx="50%"
                     cy="50%"
-                    innerRadius={40}
-                    outerRadius={80}
+                    innerRadius={20}
+                    outerRadius={45}
                   >
-                    <Cell fill={COLORS[0]} />
-                    <Cell fill={COLORS[1]} />
-                  </Pie>
-                  <Tooltip
-                    contentStyle={{ backgroundColor: '#333', border: 'none', color: '#fff' }}
-                    labelStyle={{ color: '#fff' }}
-                    itemStyle={{ color: '#fff' }}
-                  />
-                  <Legend
-                    formatter={(value) => <span style={{ color: '#fff' }}>{value}</span>}
-                  />
-                </PieChart>
+                    {data.map((_, index) => (
+                <Cell key={index} fill={COLORS[index % COLORS.length]} />
+              ))}
+            </Pie>
+            <Tooltip
+              contentStyle={{ backgroundColor: '#333', border: 'none', color: '#fff' }}
+              labelStyle={{ color: '#fff' }}
+              itemStyle={{ color: '#fff' }}
+            />
+          </PieChart>
               </ResponsiveContainer>
             </div>
-          </div>
         </div>
       ))}
     </div>
