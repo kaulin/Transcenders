9c0bd4e6d1f7e130caa39a2a4c9074c540ed6d5e
aklein <aklein@student.hive.fi>
2025-08-03 04:50:13 +0300

    feat: made a simple button that handles google-sign in, at least for starters, need to still handle tokens and stuff later, and probably can be done smarter somehow

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,68 +1,91 @@
-import { useTranslation } from "react-i18next"
-import { Link } from 'react-router-dom'
-import { useState } from "react"
-import useAuthLogin from "../hooks/useAuthLogin.ts"
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Link, useSearchParams } from 'react-router-dom';
+import useAuthLogin from '../hooks/useAuthLogin.ts';
 
 const Login = () => {
-    const { t } = useTranslation()
-	const { login } = useAuthLogin()
+  const { t } = useTranslation();
+  const { login, loginWithTokens } = useAuthLogin();
 
-    const [username, setUsername] = useState<string>("")
-    const [password, setPassword] = useState<string>("")
-	const [error, setError] = useState<string | null>(null)
+  const [username, setUsername] = useState<string>('');
+  const [searchParams] = useSearchParams();
+  const [password, setPassword] = useState<string>('');
+  const [error, setError] = useState<string | null>(null);
 
-	async function handleLogin(e: React.FormEvent) {
-		e.preventDefault()
-		setError(null)
+  useEffect(() => {
+    const error = searchParams.get('error');
+    const tokensParam = searchParams.get('tokens');
+    if (error) {
+      setError('Google auth failed');
+      window.history.replaceState({}, '', window.location.pathname);
+    }
+    if (tokensParam) {
+      const tokens = JSON.parse(decodeURIComponent(tokensParam));
+      loginWithTokens(tokens);
+    }
+  }, [searchParams, loginWithTokens]);
 
-		try {
-			await login(username, password)
-		} catch (err: any) {
-			setError(t(err.message) || t('something_went_wrong'))
-		}
-	}
+  async function handleGoogleLogin(e: React.FormEvent) {
+    e.preventDefault();
+    window.location.href = 'http://localhost:3002/auth/google';
+  }
 
-	return (
-		<div className="relative w-full h-full flex justify-center items-center pb-28">
-			<form
-				onSubmit={handleLogin}
-				className="login-bubble">
-				
-				<h1 className="text-2xl sm:text-3xl font-fascinate py-3">{t('log_in')}</h1>
+  async function handleLogin(e: React.FormEvent) {
+    e.preventDefault();
+    setError(null);
 
-				<input
-					type="text"
-					maxLength={20}
-					required
-					value={username}
-					placeholder={t("username")}
-					className="login-input-field mt-2"
-					onChange={(e) => setUsername(e.target.value)}
-					/>
+    try {
+      await login(username, password);
+    } catch (err: any) {
+      setError(t(err.localeKey) || t('something_went_wrong'));
+    }
+  }
 
-				<input
-					type="password"
-					required
-					value={password}
-					placeholder={t("password")}
-					className="login-input-field mt-2"
-					onChange={(e) => setPassword(e.target.value)}
-					/>
+  return (
+    <div className="relative w-full h-full flex justify-center items-center pb-28">
+      <form onSubmit={handleLogin} className="login-bubble">
+        <h1 className="text-2xl sm:text-3xl font-fascinate py-3">{t('log_in')}</h1>
 
-				<div className="h-4">
-					{error && <p className="text-[#786647] mt-2 text-xs sm:text-sm">{error}</p>}
-				</div>
+        <input
+          type="text"
+          maxLength={20}
+          required
+          value={username}
+          placeholder={t('username')}
+          className="login-input-field mt-2"
+          onChange={(e) => setUsername(e.target.value)}
+        />
 
-				<button type="submit" className="mt-4">
-					{t('log_in')}
-				</button>
+        <input
+          type="password"
+          required
+          value={password}
+          placeholder={t('password')}
+          className="login-input-field mt-2"
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-				<p className="mt-6">{t('new_user')}?
-					<Link to="/SignUp" className="link-color underline underline-offset-2"> {t('sign_up')} </Link>
-				</p>
-			</form>
-		</div>
-    )
-}
+        <div className="h-4">
+          {error && <p className="text-[#786647] mt-2 text-xs sm:text-sm">{error}</p>}
+        </div>
 
-export default Login
+        <button type="submit" className="mt-4">
+          {t('log_in')}
+        </button>
+        <button type="button" className="mt-4" onClick={handleGoogleLogin}>
+          {'Google Sign-in'}
+        </button>
+
+        <p className="mt-6">
+          {t('new_user')}?
+          <Link to="/SignUp" className="link-color underline underline-offset-2">
+            {' '}
+            {t('sign_up')}{' '}
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
