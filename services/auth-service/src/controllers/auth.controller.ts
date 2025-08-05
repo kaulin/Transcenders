d7aa0f238a9662ef9bc2b24df550ab7f28df4663
aklein <aklein@student.hive.fi>
2025-08-05 23:10:49 +0300

    feat: initial setup for handling multiple google auth flows via the state value

--- services/auth-service/src/controllers/auth.controller.ts
+++ services/auth-service/src/controllers/auth.controller.ts
@@ -1,7 +1,10 @@
 import {
   ApiErrorHandler,
   ChangePasswordRequest,
+  DeviceInfo,
   GoogleAuthCallback,
+  GoogleFlows,
+  GoogleUserSetPassword,
   LoginUser,
   LogoutUser,
   RefreshTokenRequest,
@@ -26,21 +29,69 @@ export class AuthController {
     return ApiErrorHandler.handleServiceResult(reply, result);
   }
 
-  static async googleAuth(request: FastifyRequest, reply: FastifyReply) {
-    const url = AuthService.getGoogleAuthUrl();
+  static async googleAuthLogin(request: FastifyRequest, reply: FastifyReply) {
+    const url = AuthService.getGoogleAuthUrl('login');
+    reply.redirect(url);
+  }
+
+  static async googleSetPassword(request: FastifyRequest, reply: FastifyReply) {
+    const url = AuthService.getGoogleAuthUrl('login');
     reply.redirect(url);
   }
 
   static async googleCallback(request: FastifyRequest, reply: FastifyReply) {
-    const { code } = request.query as GoogleAuthCallback;
+    const { code, state } = request.query as GoogleAuthCallback;
     const deviceInfo = DeviceUtils.extractDeviceInfo(request);
+
+    try {
+      switch (state) {
+        case 'login':
+          await this.handleLoginCallback(code, state, deviceInfo, reply);
+          break;
+        case 'set-password':
+          await this.handleSetPasswordCallback(code, state, reply);
+          break;
+        case 'config-change':
+          await this.handleConfigChangeCallback(code, state, reply);
+          break;
+        default:
+          reply.redirect(`${ENV.FRONTEND_URL}/auth/callback?type=error&error=invalid_state`);
+          break;
+      }
+    } catch (error) {
+      reply.redirect(`${ENV.FRONTEND_URL}/auth/callback?type=error&error=server_error`);
+    }
+  }
+
+  private static async handleLoginCallback(
+    code: string,
+    type: GoogleFlows,
+    deviceInfo: DeviceInfo,
+    reply: FastifyReply,
+  ) {
     const result = await AuthService.handleGoogleCallback(code, deviceInfo);
     if (result.success) {
       const tokens = encodeURIComponent(JSON.stringify(result.data));
-      reply.redirect(`${ENV.FRONTEND_URL}/login?tokens=${tokens}`);
+      reply.redirect(`${ENV.FRONTEND_URL}/auth/callback?type=${type}&tokens=${tokens}`);
     } else {
-      reply.redirect(`${ENV.FRONTEND_URL}/login?error=auth_failed`);
+      reply.redirect(`${ENV.FRONTEND_URL}/auth/callback?type=error&error=auth_failed`);
     }
+  }
+
+  private static async handleSetPasswordCallback(
+    code: string,
+    type: GoogleFlows,
+    reply: FastifyReply,
+  ) {
+    reply.redirect(`${ENV.FRONTEND_URL}/auth/callback?type=${type}&code=${code}`);
+  }
+
+  private static async handleConfigChangeCallback(
+    code: string,
+    type: GoogleFlows,
+    reply: FastifyReply,
+  ) {
+    reply.redirect(`${ENV.FRONTEND_URL}/auth/callback?type=${type}&code=${code}`);
   }
 
   static async logout(request: FastifyRequest, reply: FastifyReply) {
