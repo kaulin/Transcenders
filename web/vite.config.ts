2ec5abd64cb975e32194a4802d6e22483124cc4d
aklein <aklein@student.hive.fi>
2025-08-20 20:48:48 +0300

    chore: updates tailwind to v4, its a pretty big change, but I modified all the earlier utility classes to work with the new version

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,11 @@
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   cacheDir: '.vite',
+  envDir: '..',
   server: {
     port: 5173,
     host: true,
