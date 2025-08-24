ae9e26ce1859e1af890ba2223cbd659e1976d79b
Annika <makelannika@gmail.com>
2025-08-25 00:13:50 +0300

    separated user search to it's own component

--- web/src/components/SearchSection.tsx
+++ web/src/components/SearchSection.tsx
@@ -17,25 +17,27 @@ export default function SearchSection({
   const { t } = useTranslation(); 
   
   return (
-    <div className="flex flex-col">
-      <div className="flex">
-        <input
-          type="text"
-          maxLength={20}
-          value={searchedUser}
-          placeholder={t('search_user')}
-          onChange={(e) => setSearchedUser(e.target.value)}
-          className="input-field"
-        />
-        <button
-          onClick={handleSearch}
-          className="ml-4 p-2 rounded-full  border-white hover:border-[#786647] bg-white/15 text-white"
-        >
-          <ChevronRight />
-        </button>
-      </div>
-      <div className="h-6 pt-2">
-        {error && <p className="flex justify-start text-[#786647] text-xs sm:text-sm">{error}</p>}
+    <div className="flex w-full flex-col justify-start items-center">
+      <div className="flex flex-col">
+        <div className="flex">
+          <input
+            type="text"
+            maxLength={20}
+            value={searchedUser}
+            placeholder={t('search_user')}
+            onChange={(e) => setSearchedUser(e.target.value)}
+            className="input-field"
+          />
+          <button
+            onClick={handleSearch}
+            className="ml-4 p-2 rounded-full  border-white hover:border-[#786647] bg-white/15 text-white"
+          >
+            <ChevronRight />
+          </button>
+        </div>
+        <div className="h-6 pt-2">
+          {error && <p className="flex justify-start text-[#786647] text-xs sm:text-sm">{error}</p>}
+        </div>
       </div>
     </div>
   );
