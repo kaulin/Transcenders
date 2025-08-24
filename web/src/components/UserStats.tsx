e2566a852de4113a02b5c9d8423a1e7a6a7497f4
Annika <makelannika@gmail.com>
2025-08-25 01:45:07 +0300

    separated user stats to it's own component and connected to backend

--- web/src/components/UserStats.tsx
+++ web/src/components/UserStats.tsx
@@ -1,42 +1,52 @@
-// import { ApiClient } from "@transcenders/api-client";
-// import { useEffect } from "react";
-// import { useTranslation } from "react-i18next";
+import { ApiClient } from "@transcenders/api-client";
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import type { Stats } from "@transcenders/contracts";
 
-// type UserStatsProps = {
-//   userId: number | undefined;
-// };
+type UserStatsProps = {
+  userId: number | undefined;
+};
 
-// export default function UserStats({userId}: UserStatsProps) {
-//   const { t } = useTranslation();
+export default function UserStats({userId}: UserStatsProps) {
+  const { t } = useTranslation();
   
-//   useEffect(() => {
-//     async function fetchStats() {
-//       if (!userId)
-//         return;
+  const [userStats, setUserStats] = useState<Stats | undefined>(undefined);
+  const [error, setError] = useState<string | null>(null);
+  
+  useEffect(() => {
+    async function fetchStats() {
+      setError(null);
       
-//       try {
-//         const stats = await ApiClient.score.getStatsForUser(userId);
-//       } catch (err: any) {
+      if (!userId)
+        return;
+      
+      try {
+        const stats = await ApiClient.score.getStatsForUser(userId);
+        setUserStats(stats);
         
-//       }
-//     }
-//   })
+      } catch (err: any) {
+        setError(t('something_went_wrong'));
+      }
+    }
+    
+    fetchStats();
+  }, [userId]);
   
-//   return (
-//     <div className="flex flex-col font-fascinate uppercase text-center">
-//       <p className="text-2xl text-[#fff] mb-2">{t('games_played')}</p>
-//       <div className="flex justify-between text-md">
-//         <p>{t('total')}</p>
-//         <p className="font-sans">{wins + losses}</p>
-//       </div>
-//       <div className="flex justify-between text-md">
-//         <p>{t('wins')}</p>
-//         <p className="font-sans">{wins}</p>
-//       </div>
-//       <div className="flex justify-between text-md">
-//         <p>{t('losses')}</p>
-//         <p className="font-sans">{losses}</p>
-//       </div>
-//     </div>
-//   )
-// };
+  return (
+    <div className="flex flex-col font-fascinate uppercase text-center">
+      <p className="text-2xl text-[#fff] mb-2">{t('games_played')}</p>
+      <div className="flex justify-between text-md">
+        <p>{t('total')}</p>
+        <p className="font-sans">{userStats?.total_games ?? 0}</p>
+      </div>
+      <div className="flex justify-between text-md">
+        <p>{t('wins')}</p>
+        <p className="font-sans">{userStats?.total_wins ?? 0}</p>
+      </div>
+      <div className="flex justify-between text-md">
+        <p>{t('losses')}</p>
+        <p className="font-sans">{(userStats?.total_games ?? 0) - (userStats?.total_wins ?? 0)}</p>
+      </div>
+    </div>
+  );
+};
