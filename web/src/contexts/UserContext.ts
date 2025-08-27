5c3c743b858eb28e64ceac242fca509e175fd24e
aklein <aklein@student.hive.fi>
2025-08-27 03:03:57 +0300

    Refactor UserContext to remove updateUser function

--- web/src/contexts/UserContext.ts
+++ web/src/contexts/UserContext.ts
@@ -1,10 +1,9 @@
-import type { UpdateUserRequest, User } from '@transcenders/contracts';
+import type { User } from '@transcenders/contracts';
 import { createContext } from 'react';
 
 interface UserContextType {
   user: User | null;
   setUser: React.Dispatch<React.SetStateAction<User | null>>;
-  updateUser: (userData: UpdateUserRequest) => Promise<User | null>;
 }
 
 export const UserContext = createContext<UserContextType | undefined>(undefined);
