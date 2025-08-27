89deae9fc07149db45903149048b004cb1c77420
aklein <aklein@student.hive.fi>
2025-08-28 01:44:29 +0300

    Add error handler for cookie authentication failures

--- packages/fastify-server/src/hooks/error.hook.ts
+++ packages/fastify-server/src/hooks/error.hook.ts
@@ -1,10 +1,14 @@
 import { ApiErrorHandler } from '@transcenders/contracts';
+import { CookieUtils } from '@transcenders/server-utils';
 import { FastifyError, FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 
 export function registerErrorHandler(fastify: FastifyInstance) {
   fastify.setErrorHandler(
     async (error: FastifyError, request: FastifyRequest, reply: FastifyReply) => {
       const result = ApiErrorHandler.errorFromFastify(reply, error, 'fastify error handler');
+      if (!result.success && result.error.httpStatus == 410) {
+        CookieUtils.clearCookies(reply);
+      }
       return result;
     },
   );
