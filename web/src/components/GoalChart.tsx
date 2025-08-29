327774c00373ab30fb009ff9cb56a6ad1659dc38
Annika <makelannika@gmail.com>
2025-08-30 01:18:31 +0300

    minor fixes

--- web/src/components/GoalChart.tsx
+++ web/src/components/GoalChart.tsx
@@ -31,7 +31,7 @@ export default function GoalChart({userId}: GoalChartProps) {
     ApiClient.score
       .getScoresForUser(userId)
       .then(setUserScores)
-      .catch((err: any) => setError(err?.localeKey ?? 'something_went_wrong'));
+      .catch((err: any) => setError(t(err.localeKey ?? 'something_went_wrong')));
   }, [userId, t]);
 
   const chartData = userScores
@@ -46,55 +46,67 @@ export default function GoalChart({userId}: GoalChartProps) {
   if (!userScores) return <div className="tsc-info-message text-center">{t('loading')}</div>;
   
   return (
-    <div className="flex w-full justify-center items-center h-40 pr-10">
-      <ResponsiveContainer width="80%" height="80%">
-        <AreaChart data={chartData}>
-          <defs>
-            <linearGradient id="scored" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#a7d4373c" stopOpacity={1} />
-              <stop offset="95%" stopColor="#fffb82" stopOpacity={0.2} />
-            </linearGradient>
-            <linearGradient id="conceded" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#5d6b2f52" stopOpacity={0.6} />
-              <stop offset="95%" stopColor="#5d6b2f52" stopOpacity={0} />
-            </linearGradient>
-          </defs>
-          <CartesianGrid strokeDasharray="3 3" stroke="#ffffff33" />
-          <XAxis
-            dataKey="date"
-            stroke="#fff"
-            tick={{ fontSize: 12, fill: '#fff' }}
-            interval="preserveStartEnd"
-            tickFormatter={(value) =>
-              new Intl.DateTimeFormat('en-GB', { day: '2-digit', month: '2-digit' }).format(
-                new Date(value),
-              )
-            }
-          />
-          <YAxis stroke="#fff" />
-          <Tooltip
-            contentStyle={{ backgroundColor: '#333', border: 'none', color: '#fff' }}
-            labelStyle={{ color: '#fff' }}
-            itemStyle={{ color: '#fff' }}
-          />
-          <Area
-            type="monotone"
-            dataKey="conceded"
-            stroke="#fff"
-            fillOpacity={1}
-            fill="url(#conceded)"
-            name={t('opponent')}
-          />
-          <Area
-            type="monotone"
-            dataKey="scored"
-            stroke="#fff"
-            fillOpacity={1}
-            fill="url(#scored)"
-            name={t('you')}
-          />
-        </AreaChart>
-      </ResponsiveContainer>
+    <div className="flex flex-col items-center w-full">
+      <div className="flex w-full justify-center items-center h-40 pr-10">
+        <ResponsiveContainer width="80%" height="80%">
+          <AreaChart data={chartData}>
+            <defs>
+              <linearGradient id="scored" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#a7d4373c" stopOpacity={1} />
+                <stop offset="95%" stopColor="#fffb82" stopOpacity={0.2} />
+              </linearGradient>
+              <linearGradient id="conceded" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#5d6b2f52" stopOpacity={0.6} />
+                <stop offset="95%" stopColor="#5d6b2f52" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <CartesianGrid strokeDasharray="3 3" stroke="#ffffff33" />
+            <XAxis
+              dataKey="date"
+              stroke="#fff"
+              tick={{ fontSize: 12, fill: '#fff' }}
+              interval="preserveStartEnd"
+              tickFormatter={(value) =>
+                new Intl.DateTimeFormat('en-GB', { day: '2-digit', month: '2-digit' }).format(
+                  new Date(value),
+                )
+              }
+            />
+            <YAxis stroke="#fff" />
+            <Tooltip
+              contentStyle={{ backgroundColor: '#333', border: 'none', color: '#fff' }}
+              labelStyle={{ color: '#fff' }}
+              itemStyle={{ color: '#fff' }}
+            />
+            <Area
+              type="monotone"
+              dataKey="conceded"
+              stroke="#fff"
+              fillOpacity={1}
+              fill="url(#conceded)"
+              name={t('opponent')}
+            />
+            <Area
+              type="monotone"
+              dataKey="scored"
+              stroke="#fff"
+              fillOpacity={1}
+              fill="url(#scored)"
+              name={t('you')}
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
+      <div className="flex flex-col sm:flex-row text-sm sm:text-base gap-4">
+        <div className="flex gap-2 items-center text-white">
+          <div className="w-4 h-4 rounded-full bg-[#a7d4373c] border border-white"></div>
+          {t('you')}
+        </div>
+        <div className="flex gap-2 items-center text-white">
+          <div className="w-4 h-4 rounded-full bg-[#5d6b2f52] border border-white"></div>
+          {t('opponent')}
+        </div>
+      </div>
     </div>
   );
 };
