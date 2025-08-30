b0d2b56ca1e9b126b945989abf732eed9c970bfb
aklein <aklein@student.hive.fi>
2025-08-30 22:08:11 +0300

    feat: added a different type of login that only checks for the userLogin info, and doesn't give any tokens for it, to do the login check before match start
    also returning userId in the AuthData return that comes back from login now

--- web/src/hooks/useVerifyLogin.ts
+++ web/src/hooks/useVerifyLogin.ts
@@ -1,5 +1,5 @@
 import { ApiClient } from '@transcenders/api-client';
-import { decodeToken, type LoginUser } from '@transcenders/contracts';
+import { type LoginUser } from '@transcenders/contracts';
 import { usePlayers } from '../hooks/usePlayers';
 import { useApiClient } from './useApiClient';
 
@@ -8,11 +8,11 @@ const useVerifyLogin = () => {
   const api = useApiClient();
 
   async function login(username: string, password: string, playerNumber: number) {
-    const loginInfo: LoginUser = { username, password };
-    const authData = await api(() => ApiClient.auth.login(loginInfo));
-    const payload = decodeToken(authData.accessToken);
+    // by setting onlyVerify to true, we only check for password to be correct and set no cookies or anything
+    const verifyLoginInfo: LoginUser = { username, password, onlyVerify: true };
+    const authData = await api(() => ApiClient.auth.login(verifyLoginInfo));
 
-    const user = await api(() => ApiClient.user.getUserById(payload.userId));
+    const user = await api(() => ApiClient.user.getUserById(authData.userId));
 
     setPlayer(playerNumber, {
       id: user.id,
