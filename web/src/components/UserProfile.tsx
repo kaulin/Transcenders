972d612c6ed078b820816ba35ce84b2de95744a8
Annika <makelannika@gmail.com>
2025-08-31 22:24:20 +0300

    dashboard user section

---
+++ web/src/components/UserProfile.tsx
@@ -0,0 +1,26 @@
+import { ApiClient } from "@transcenders/api-client";
+import { User } from "@transcenders/contracts";
+
+type UserProfileProps = {
+  user: User | null;
+};
+
+export default function UserProfile({ user }: UserProfileProps) {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="bubble bg-white/50 w-56 h-56 flex items-end justify-center overflow-hidden">
+        {user?.avatar ? (
+          <img
+            src={ApiClient.user.getFullAvatarURL(user.avatar)}
+            alt={`${user.username} avatar`}
+            className="object-cover"
+            onError={(e) => {
+              e.currentTarget.style.display = 'none';
+            }}
+          />
+        ) : null}
+      </div>
+      <h1 className="pt-6 text-5xl text-[#fff] font-fascinate">{user?.username}</h1>
+    </div>
+  );
+};
---
+++ web/src/components/UserSearch.tsx
@@ -0,0 +1,44 @@
+import { ChevronRight } from "lucide-react";
+import { useTranslation } from "react-i18next";
+
+interface UserSearchProps {
+  searchedUser: string;
+  setSearchedUser: (value: string) => void;
+  handleSearch: () => void;
+  error: string | null;
+}
+
+export default function UserSearch({
+  searchedUser,
+  setSearchedUser,
+  handleSearch,
+  error
+}: UserSearchProps) {
+  const { t } = useTranslation(); 
+  
+  return (
+    <div className="flex w-full flex-col justify-start items-center">
+      <div className="flex flex-col">
+        <div className="flex">
+          <input
+            type="text"
+            maxLength={20}
+            value={searchedUser}
+            placeholder={t('search_user')}
+            onChange={(e) => setSearchedUser(e.target.value)}
+            className="input-field"
+          />
+          <button
+            onClick={handleSearch}
+            className="ml-4 p-2 rounded-full  border-white hover:border-[#786647] bg-white/15 text-white"
+          >
+            <ChevronRight />
+          </button>
+        </div>
+        <div className="h-6 pt-2">
+          {error && <p className="flex justify-start text-[#786647] text-xs sm:text-sm">{error}</p>}
+        </div>
+      </div>
+    </div>
+  );
+};
