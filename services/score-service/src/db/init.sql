7aa804a549a966b32d2240d585621a0263e00c29
Jaakko Junttila <junttila@proton.me>
2025-09-04 16:55:33 +0300

    Adds database level constraints for score data

--- services/score-service/src/db/init.sql
+++ services/score-service/src/db/init.sql
@@ -9,4 +9,9 @@ CREATE TABLE IF NOT EXISTS scores (
   game_start TIMESTAMP NOT NULL,
   game_end TIMESTAMP NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  
+  CHECK (game_end > game_start),
+  CHECK (game_duration = game_end - game_start),
+  CHECK (tournament_level >= 0 AND tournament_level <= 2),
+  CHECK (winner_id != loser_id OR (winner_id = 0 AND loser_id = 0))
 );
