cbbb156c79606afa3b3ffc53daf6004b33bcb29b
Jaakko Junttila <junttila@proton.me>
2025-09-05 11:00:38 +0300

    Adds check for negative scores

--- services/score-service/src/db/init.sql
+++ services/score-service/src/db/init.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS scores (
   
   CHECK (winner_id != loser_id OR (winner_id = 0 AND loser_id = 0)),
   CHECK (winner_score = 3),
-  CHECK (loser_score < 3),
+  CHECK (loser_score >= 0 AND loser_score < 3),
   CHECK (game_end > game_start),
   CHECK (game_duration = (julianday(game_end) - julianday(game_start)) * 86400),
   CHECK (tournament_level >= 0 AND tournament_level <= 2),
