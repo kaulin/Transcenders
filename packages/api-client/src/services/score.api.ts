9b7b617e43434fc0e08b5f117cba6c8786b91ded
Jaakko Junttila <junttila@proton.me>
2025-09-08 08:48:34 +0300

    Makes match creation only return the match id. Standardizes schema naming.

--- packages/api-client/src/services/score.api.ts
+++ packages/api-client/src/services/score.api.ts
@@ -1,14 +1,14 @@
 import { Static, TSchema } from '@sinclair/typebox';
 import {
   CreateMatchRequest,
+  createMatchResultSchema,
   CreateScoreRequest,
   GetScoresQuery,
-  MatchSchema,
   SCORE_ROUTES,
   scoreArraySchema,
-  ScoreSchema,
+  scoreSchema,
   SERVICE_URLS,
-  StatsSchema,
+  statsSchema,
   toQueryString,
 } from '@transcenders/contracts';
 import { TypedApiClient } from '../api/TypedApiClient.js';
@@ -42,7 +42,7 @@ export class ScoreApiClient extends TypedApiClient {
   static async createScore(scoreData: CreateScoreRequest) {
     const endpoint = SCORE_ROUTES.SCORES;
     const options: ApiCallOptions = { method: 'POST', body: scoreData };
-    return this.callScoreService(endpoint, ScoreSchema, options);
+    return this.callScoreService(endpoint, scoreSchema, options);
   }
 
   /**
@@ -51,7 +51,7 @@ export class ScoreApiClient extends TypedApiClient {
   static async createMatch(matchData: CreateMatchRequest) {
     const endpoint = SCORE_ROUTES.MATCH;
     const options: ApiCallOptions = { method: 'POST', body: matchData };
-    return this.callScoreService(endpoint, MatchSchema, options);
+    return this.callScoreService(endpoint, createMatchResultSchema, options);
   }
 
   /**
@@ -67,6 +67,6 @@ export class ScoreApiClient extends TypedApiClient {
    */
   static async getStatsForUser(userId: number) {
     const endpoint = SCORE_ROUTES.STATS_BY_ID.replace(':id', userId.toString());
-    return this.callScoreService(endpoint, StatsSchema);
+    return this.callScoreService(endpoint, statsSchema);
   }
 }
