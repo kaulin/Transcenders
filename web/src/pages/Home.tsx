5cfaa164e6de72489100fc67260912eb37f0b1bd
Annika <makelannika@gmail.com>
2025-09-11 13:18:12 +0300

    responsive home page with fluid text sizes

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -5,15 +5,13 @@ import { useTranslation } from 'react-i18next';
 import PlayerLoginForm from '../components/PlayerLoginForm';
 import { usePlayers } from '../hooks/usePlayers';
 import { useUser } from '../hooks/useUser';
-// import curiousCat from "/images/curiousCat.avif"
 
 const Home = () => {
   const { t } = useTranslation();
   const { user } = useUser();
   const { players, setPlayer } = usePlayers();
 
-  const playerFormRef = useRef<HTMLDivElement | null>(null);
-  const [gameMode, setGameMode] = useState<'match' | 'tournament' | null>(null);
+  const [gameMode, setGameMode] = useState<'match' | 'tournament' | 'none'>('none');
 
   useEffect(() => {
     if (user?.id) {
@@ -27,117 +25,106 @@ const Home = () => {
     }
   }, [user?.id, user?.username, user?.avatar, setPlayer]);
 
-  const handleGameMode = (mode: 'match' | 'tournament') => {
-    setGameMode(mode);
-
-    setTimeout(() => {
-      if (window.innerWidth < 1300 && playerFormRef.current) {
-        playerFormRef.current.scrollIntoView({
-          behavior: 'smooth',
-          block: 'center',
-        });
-      }
-    }, 100);
-  };
-
   return (
-    <div className="relative w-full h-full">
-      <div className="box relative bg-[#6e5d41]/10 xl:bg-[#6e5d41]/5">
-        <div className="flex flex-col xl:flex-1 shrink-0 w-full min-h-[1106px] h-full p-10 items-center justify-center text-center xl:bg-transparent backdrop-blur-sm">
-          <h1 className="text-6xl font-fascinate">
+    <div className="box">
+      {gameMode === 'none' && (
+        <div className="box-section bg-[#6e5d41]/10 justify-center lg:min-h-0">
+          <h1 className="text-fluid-5xl text-center font-fascinate">
             {t('hello')} {user?.username}
           </h1>
-          <p className="mb-10 text-xl">{t('welcome')}!</p>
+          <p className="mb-[clamp(23px,1.6vw,40px)] text-fluid-base">{t('welcome')}!</p>
 
           <div className="flex flex-col">
             <button
-              onClick={() => handleGameMode('match')}
-              className="rounded-button bg-[#6e5d41]/15 text-xs sm:text-base font-fascinate uppercase mt-6"
+              onClick={() => setGameMode('match')}
+              className="rounded-button bg-[#6e5d41]/10 text-fluid-xs font-fascinate uppercase mt-[clamp(14px,0.94vw,24px)]"
             >
               {t('start_match')}
             </button>
 
             <button
-              onClick={() => handleGameMode('tournament')}
-              className="rounded-button bg-[#6e5d41]/15 text-xs sm:text-base font-fascinate uppercase mt-6"
+              onClick={() => setGameMode('tournament')}
+              className="rounded-button bg-[#6e5d41]/10 text-fluid-xs font-fascinate uppercase mt-[clamp(14px,0.94vw,24px)]"
             >
               {t('start_tournament')}
             </button>
           </div>
         </div>
+      )}
 
-        <div className="relative flex flex-col xl:flex-1 shrink-0 min-h-[1106px] w-full h-full items-center justify-center gap-2 backdrop-blur-sm">
-          {gameMode === 'match' && (
-            <div
-              ref={playerFormRef}
-              className="box-section xl:bg-[#6e5d41]/10 justify-center xl:max-w-[790px]"
-            >
-              <div className="w-full max-w-[384px] h-[174px] p-4">
-                <h2 className="fascinate-label mb-3">{t('player')} 1</h2>
-                <p className="border-b-2 border-white text-lg flex justify-between">
-                  {user?.username}
-                </p>
-                <p className="flex justify-end text-lg mt-2 p-2">✓</p>
-              </div>
-              <div className="w-full max-w-[384px] h-[174px] p-4">
-                <PlayerLoginForm playerNumber={2} player={players[2]} />
-              </div>
+      {gameMode === 'match' && (
+        <div className="box-section bg-[#6e5d41]/10 justify-center">
+          <h1 className="text-fluid-4xl font-fascinate uppercase">{t('one_v_one')}</h1>
+          <button onClick={() => setGameMode('none')} className="mb-[clamp(40px,3.7vw,90px)]">
+            {t('switch_game_mode')}
+          </button>
+          <div className="flex flex-col sm:flex-row justify-center sm:gap-10">
+            <div className="w-[clamp(250px,15.6vw,320px)] h-[clamp(100px,13.35vh,174px)] p-[clamp(9px,0.63vw,16px)] text-center">
+              <h2 className="fascinate-label">{t('player')} 1</h2>
+              <p className="border-b-2 border-white text-fluid-base flex justify-start">
+                {user?.username}
+              </p>
+              <p className="flex justify-end text-fluid-sm mt-[clamp(5px,0.61vh,8px)] p-[clamp(5px,0.31vw,8px)]">
+                ✓
+              </p>
             </div>
-          )}
-
-          {gameMode === 'tournament' && (
-            <div
-              ref={playerFormRef}
-              className="box-section xl:bg-[#6e5d41]/10 justify-center xl:max-w-[790px]"
-            >
-              <div className="w-full max-w-[384px] h-[174px] p-4">
-                <h2 className="fascinate-label mb-3">{t('player')} 1</h2>
-                <p className="border-b-2 border-white text-lg flex justify-between">
-                  {user?.username}
-                </p>
-                <p className="flex justify-end text-lg mt-2 p-2">✓</p>
-              </div>
-              <div className="w-full max-w-[384px] h-[174px] p-4">
-                <PlayerLoginForm playerNumber={2} player={players[2]} />
-              </div>
-              <div className="w-full max-w-[384px] h-[174px] p-4">
-                <PlayerLoginForm playerNumber={3} player={players[3]} />
-              </div>
-              <div className="w-full max-w-[384px] h-[174px] p-4">
-                <PlayerLoginForm playerNumber={4} player={players[4]} />
-              </div>
+            <div className="w-[clamp(250px,15.6vw,320px)] h-[clamp(130px,13.35vh,174px)] p-[clamp(9px,0.63vw,16px)] text-center">
+              <PlayerLoginForm playerNumber={2} player={players[2]} />
             </div>
-          )}
-          <div className="absolute bottom-[130px]">
-            {gameMode === 'tournament' &&
-              players[2]?.ready &&
-              players[3]?.ready &&
-              players[4]?.ready && (
-                <Link
-                  to="/TournamentPage"
-                  className="rounded-button bg-[#6e5d41]/15 font-fascinate uppercase"
-                >
-                  start
-                </Link>
-              )}
-
-            {gameMode === 'match' && players[2]?.ready && (
+          </div>
+          <div className="flex justify-center h-[36px]">
+            {players[2]?.ready && (
               <Link
                 to="/MatchPage"
-                className="rounded-button bg-[#6e5d41]/15 font-fascinate uppercase"
+                className="font-fascinate uppercase text-fluid-xl text-white hover:text-[#786647] mt-[clamp(37px,2.8vw,72px)]"
               >
                 start
               </Link>
             )}
           </div>
         </div>
-      </div>
+      )}
 
-      {/* <img
-				src={curiousCat}
-				alt="curiousCat"
-				className="absolute left-0 bottom-0 translate-y-[40.5%] translate-x-[0.5%] w-[250px] sm:w-[325px] lg:w-[400px] object-contain pointer-events-none"
-			/> */}
+      {gameMode === 'tournament' && (
+        <div className="box-section bg-[#6e5d41]/10 justify-center">
+          <h1 className="text-fluid-3xl font-fascinate uppercase">{t('tournament')}</h1>
+          <button onClick={() => setGameMode('none')} className="mb-[clamp(40px,3.7vw,90px)]">
+            {t('switch_game_mode')}
+          </button>
+          <div className="flex flex-col justify-center sm:gap-10">
+            <div className="flex flex-col sm:flex-row justify-center sm:gap-10">
+              <div className="w-[clamp(250px,15.6vw,320px)] h-[clamp(100px,13.35vh,174px)] p-[clamp(9px,0.63vw,16px)] text-center">
+                <h2 className="fascinate-label mb-[clamp(5px,0.61vh,8px)]">{t('player')} 1</h2>
+                <p className="border-b-2 border-white text-fluid-base flex justify-between">
+                  {user?.username}
+                </p>
+                <p className="flex justify-end text-fluid-sm pt-[clamp(9px,0.63vw,16px)]">✓</p>
+              </div>
+              <div className="w-[clamp(250px,15.6vw,320px)] h-[clamp(130px,13.35vh,174px)] p-[clamp(9px,0.63vw,16px)] text-center">
+                <PlayerLoginForm playerNumber={2} player={players[2]} />
+              </div>
+            </div>
+            <div className="flex flex-col sm:flex-row justify-center sm:gap-10">
+              <div className="w-[clamp(250px,15.6vw,320px)] h-[clamp(130px,13.35vh,174px)] p-[clamp(9px,0.63vw,16px)] text-center">
+                <PlayerLoginForm playerNumber={3} player={players[3]} />
+              </div>
+              <div className="w-[clamp(250px,15.6vw,320px)] h-[clamp(130px,13.35vh,174px)] p-[clamp(9px,0.63vw,16px)] text-center">
+                <PlayerLoginForm playerNumber={4} player={players[4]} />
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-center h-[36px]">
+            {players[2]?.ready && players[3]?.ready && players[4]?.ready && (
+              <Link
+                to="/TournamentPage"
+                className="font-fascinate uppercase text-fluid-xl text-white hover:text-[#786647] mt-[clamp(37px,2.8vw,72px)]"
+              >
+                start
+              </Link>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
